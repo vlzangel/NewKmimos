fe59164ac6d13482ad060813f2c4bb8b8e3785d7
etpro01 <etpro@etprogroup.com>
2017-09-14 18:01:19 -0400

    subir act rupeerar contrase;a

---
+++ wp-content/themes/kmimos/procesos/login/recuperar.php
@@ -0,0 +1,104 @@
+<?php
+define('WP_USE_THEMES', false);
+//include_once(ABSPATH."vlz_config.php");
+$config = dirname(__DIR__,5)."/wp-config.php";
+if(file_exists($config)){
+    include_once($config);
+}
+
+add_filter( 'wp_mail_content_type','recover_set_content_type' );
+function recover_set_content_type(){
+    return "text/html";
+}
+
+
+$email = $_POST['email'];
+$conn = new mysqli($host, $user, $pass, $db);
+$message = '';
+
+
+$user_data="";
+if(empty($email)){
+    echo 'email Vacio';
+    exit();
+
+}else if(strpos($email,'@')){
+    $user_data = get_user_by( 'email', trim($email));
+
+    if(empty($user_data)){
+        echo 'email no existe';
+        exit();
+    }
+
+} else {
+    $login = trim($email);
+    $user_data = get_user_by('login', $login);
+
+    if(empty($user_data)){
+        echo 'usuario no existe';
+        exit();
+    }
+
+}
+
+
+if (!$user_data){
+    echo 'error en datos';
+    exit();
+}
+
+
+// redefining user_login ensures we return the right case in the email
+$user_login = $user_data->user_login;
+$user_email = $user_data->user_email;
+$user_name = $user_data->user_name;
+
+do_action('retrieve_password', $user_login);
+$allow = apply_filters('allow_password_reset', true, $user_data->ID);
+
+if (!$allow){
+    return false;
+}else if (is_wp_error($allow)){
+    echo 'error en datos';
+    exit();
+}
+
+$key = '';//$wpdb->get_var($wpdb->prepare("SELECT user_activation_key FROM $wpdb->users WHERE user_login = %s", $user_login));
+$keyKmimos=md5($user_data->ID);
+update_user_meta($user_data->ID, 'clave_temp', $keyKmimos);
+
+if ( empty($key) ) {
+
+    $key = wp_generate_password(20, false);
+    do_action('retrieve_password_key', $user_login, $key);
+
+    if ( empty( $wp_hasher ) ) {
+        require_once(ABSPATH.'wp-includes/class-phpass.php');
+        $wp_hasher = new PasswordHash( 8, true );
+    }
+
+    //Change this
+    //$hashed = $wp_hasher->HashPassword( $key );
+    $hashed = time().':'.$wp_hasher->HashPassword($key);
+    $wpdb->update($wpdb->users, array('user_activation_key' => $hashed), array('user_login' => $user_login));
+}
+
+$url_activate=site_url()."/wp-login.php?action=rp&key=$key&login=".rawurlencode($user_login);
+$url_activate=site_url()."/restablecer/?r=".$keyKmimos;
+
+//MESSAGE
+$mail_file=dirname(dirname(__DIR__)).'/template/mail/recuperar.php';
+$message_mail=file_get_contents($mail_file);
+$message_mail=str_replace('[name]',$user_login,$message_mail);
+$message_mail=str_replace('[url]',$url_activate,$message_mail);
+//$message_mail=htmlentities($message_mail);
+
+//MAIL
+$subjet='Cambiar contraseña en Kmimos';
+$message=kmimos_get_email_html($subjet, $message_mail, 'Saludos,', false, true);
+wp_mail($user_email,  $subjet, $message);
+
+echo 'Hemos enviado los pasos para restablecer la contraseña a tu correo.';
+exit();
+
+?>
---
+++ wp-content/themes/kmimos/template/mail/recuperar.php
@@ -0,0 +1,34 @@
+<h1>¡Recuperación de Contraseña!</h1>
+<p style="text-align: justify;">Hola <strong>[name]</strong>,</p>
+<p style="text-align: justify;">
+    Hemos recibido tu solicitud para restablecer tu contraseña en Kmimos.
+</p>
+<p style="text-align: justify;">
+    Como parte del proceso de mejoras que tenemos en Kmimos, hemos hecho un cambio en la plataforma con una serie de beneficios que fueron compartidos en un email aparte.
+</p>
+<p style="text-align: justify;">
+    Para restablecer tu contraseña por favor pícale al botón de abajo.
+</p>
+<p style="text-align: center;">
+    <a  target="_blank"
+        href="[url]"
+        style="
+                        padding: 10px;
+                        background: #59c9a8;
+                        color: #fff;
+                        font-weight: 400;
+                        font-size: 17px;
+                        font-family: Roboto;
+                        border-radius: 3px;
+                        border: solid 1px #1f906e;
+                        display: block;
+                        max-width: 300px;
+                        margin: 0px auto;
+                        text-align: center;
+                        text-decoration: none;
+                    "
+        >Pícale para restablecer la contraseña</a>
+</p>
+<p style="text-align: justify;">
+    <strong>Si no has solicitado cambiar tu contraseña, no te preocupes, solo ignora este correo y tu actual contraseña permanecerá activa.</strong>
+</p>
