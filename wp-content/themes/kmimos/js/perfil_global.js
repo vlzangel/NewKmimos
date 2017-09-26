4e36cbc334f8c872c80a2fcd1773ff33ba1e82d5
Angel Veloz <vlzangel91@gmail.com>
2017-09-26 18:47:21 +0200

    Ajustes perfil

--- wp-content/themes/kmimos/js/perfil_global.js
+++ wp-content/themes/kmimos/js/perfil_global.js
@@ -1,7 +1,7 @@
 jQuery( document ).ready(function() {
   
-  jQuery(".navbar").removeClass("bg-transparent");
+/*  jQuery(".navbar").removeClass("bg-transparent");
   jQuery(".navbar").addClass("bg-white-secondary");
-  jQuery('.navbar-brand img').attr('src', HOME+'/images/new/km-logos/km-logo-negro.png');
+  jQuery('.navbar-brand img').attr('src', HOME+'/images/new/km-logos/km-logo-negro.png');*/
 
 });
