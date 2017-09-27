402c81fc64f9506e5313a038edf3e26df7c82df9
Italo <italococchini@gmail.com>
2017-09-27 15:32:14 -0400

    ajustes redes sociales

--- wp-content/themes/kmimos/header.php
+++ wp-content/themes/kmimos/header.php
@@ -41,8 +41,8 @@
 		$reserrvacion_page = "page-reservation";
 	}
 
-    // include_once("partes/head/script_google_auth.php");
-    // include_once("partes/head/script_facebook_auth.php");
+    include_once("partes/head/script_google_auth.php");
+    include_once("partes/head/script_facebook_auth.php");
 
     $coordenadas = get_coordenadas();
     $HTML .= "<script type='text/javascript'>
