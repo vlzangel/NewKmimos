d826ba22e0845b3796a76fcb775f3d701943eaca
Angel Veloz <vlzangel91@gmail.com>
2017-10-20 18:56:18 +0200

    Act

--- wp-content/themes/kmimos/template/mail/registro.php
+++ wp-content/themes/kmimos/template/mail/registro.php
@@ -2,12 +2,11 @@
     <img src='[URL_IMGS]/header1.png' style='width: 100%;' >
 </div>
 
-
 <div style='margin-bottom: 30px; font-size: 14px; line-height: 1.07; letter-spacing: 0.3px; color: #000000;'>
-    <p style='font-family: Arial; font-size: 20px; font-weight: bold; letter-spacing: 0.4px; color: #6b1c9b; padding-bottom: 15px;'>
+    <div style='font-family: Arial; font-size: 20px; font-weight: bold; letter-spacing: 0.4px; color: #6b1c9b; padding-bottom: 15px;'>
         ¡Hola [name]!
-    </p>    
-    <p style='max-width: 518px; width: 100%;'>Te entregamos tus credenciales para que tengas acceso a Kmimos: </p>
+    </div>    
+    <div style='max-width: 518px; width: 100%;'>Te entregamos tus credenciales para que tengas acceso a Kmimos: </div>
 </div>
 
 <div style='display: block; max-width: 400px; width: 100%; margin: 0px auto; margin-bottom: 31px;'>
@@ -17,7 +16,7 @@
         </div>
         <div style='display: table-cell; vertical-align: middle; padding-left: 16px;'>
             <div style='font-size: 11px; font-weight: bold; letter-spacing: -0.1px; color: #0d7ad9;'>E-mail</div>                   
-            <div style='font-family: Arial; font-size: 16px; font-weight: bold; letter-spacing: -0.5px; color: #000000; margin-bottom: 12px;'><a href="mailto:[email]">[email]</a></div>
+            <div style='font-family: Arial; font-size: 16px; font-weight: bold; letter-spacing: -0.5px; color: #000000; margin-bottom: 12px;'><a href="mailto:[email]" style="color: #000; text-decoration: none;">[email]</a></div>
 
 
             <div style='font-family: Arial; font-size: 11px; font-weight: bold; letter-spacing: -0.1px; color: #0d7ad9;'>Contraseña</div>
@@ -26,6 +25,6 @@
     </div>
 
     <div style='text-align: center;'>
-        <a href="[url]"><img src='[URL_IMGS]/btn_iniciar_session.png' ></a>
+        <a href="[url]" style="color: #000; text-decoration: none;"><img src='[URL_IMGS]/btn_iniciar_session.png' ></a>
     </div>
 </div>
