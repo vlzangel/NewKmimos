a073d39d84d27dae69ba0351dcbc8cc2713c3924
Angel Veloz <vlzangel91@gmail.com>
2017-10-24 20:41:56 -0430

    Correos solicitud

--- wp-content/themes/kmimos/procesos/conocer/cancelar.php
+++ wp-content/themes/kmimos/procesos/conocer/cancelar.php
@@ -1,5 +1,9 @@
 <?php
 	
+    
+    $wpdb->query("UPDATE wp_postmeta SET meta_value = '3' WHERE post_id = $id_orden AND meta_key = 'request_status';");
+    $wpdb->query("UPDATE wp_posts SET post_status = 'draft' WHERE ID = '{$id_orden}';");
+
 	$cuidador_info = $cuidador;
 
 	$sql = "
@@ -83,11 +87,30 @@
     $mensaje_cliente = str_replace('[name_cliente]', $cliente_name, $mensaje_cliente);
     $mensaje_cliente = str_replace('[name_cuidador]', $cuidador_name, $mensaje_cliente);
     $mensaje_cliente = str_replace('[CUIDADORES]', $str_sugeridos, $mensaje_cliente);
-
-
-    
     $mensaje_cliente = str_replace('[URL_IMGS]', get_home_url()."/wp-content/themes/kmimos/images/emails", $mensaje_cliente);
 
-	echo get_email_html( $mensaje_cliente );	
+	$mensaje_cliente = get_email_html( $mensaje_cliente );	
+
+    // wp_mail( $email_cliente, "Cancelación de Solicitud para conocer cuidador", $mensaje_cliente);
+
+
+
+    $file = $PATH_TEMPLATE.'/template/mail/conocer/cancelar_cuidador.php';
+    $mensaje_cuidador = file_get_contents($file);
+
+    $mensaje_cuidador = str_replace('[id_reserva]', $id_orden, $mensaje_cuidador);
+    $mensaje_cuidador = str_replace('[name_cuidador]', $cuidador_name, $mensaje_cuidador);
+    $mensaje_cuidador = str_replace('[URL_IMGS]', get_home_url()."/wp-content/themes/kmimos/images/emails", $mensaje_cuidador);
+
+    $mensaje_cuidador = get_email_html( $mensaje_cuidador );  
+
+    // wp_mail( $email_cliente, "Cancelación de Solicitud para conocer cuidador", $mensaje_cuidador);
+
+    if( $_GET["user"] == "CLI" ){
+        echo $mensaje_cliente;
+    }else{
+        echo $mensaje_cuidador;
+    }
+
 ?>
 
