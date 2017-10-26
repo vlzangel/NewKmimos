f4d008d8a36eb83568fe15cccc26081d9be58fc6
Italo <italococchini@gmail.com>
2017-10-26 16:34:36 -0400

    ajustes solo iOS

--- wp-content/themes/kmimos/pre-header.php
+++ wp-content/themes/kmimos/pre-header.php
@@ -68,11 +68,8 @@
 		$iPod    = stripos($_SERVER['HTTP_USER_AGENT'],"iPod");
 		$iPhone  = stripos($_SERVER['HTTP_USER_AGENT'],"iPhone");
 		$iPad    = stripos($_SERVER['HTTP_USER_AGENT'],"iPad");
-		$Android = stripos($_SERVER['HTTP_USER_AGENT'],"Android");
 		$webOS   = stripos($_SERVER['HTTP_USER_AGENT'],"webOS");
-		if( $iPod || $iPhone || $iPad || $Android || $webOS){
+		if( $iPod || $iPhone || $iPad || $webOS){
 			$is_iOS = true;
 		}
 	}
-
-    
