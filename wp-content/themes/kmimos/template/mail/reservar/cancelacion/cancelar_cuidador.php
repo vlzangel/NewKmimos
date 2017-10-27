e5c419a3cb4026de17922e42a88da34cf8a97c4c
Angel Veloz <vlzangel91@gmail.com>
2017-10-27 18:51:48 +0200

    Modificacion de

--- wp-content/themes/kmimos/template/mail/reservar/cancelacion/cancelar_cuidador.php
+++ wp-content/themes/kmimos/template/mail/reservar/cancelacion/cancelar_cuidador.php
@@ -5,6 +5,9 @@
 <div style='padding: 0px;'>
 
 	<div style='font-size: 14px; line-height: 1.07; letter-spacing: 0.3px; color: #000000;'>
+        
+        [MODIFICACION] 
+
 		<div style='font-family: Arial; font-size: 20px; font-weight: bold; letter-spacing: 0.4px; color: #6b1c9b; padding-bottom: 10px;'>
 			Hola [name_cuidador]
 		</div>	
