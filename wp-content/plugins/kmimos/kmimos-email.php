7e61f28b7e80546a1a8fbd616b03aff16a55fde6
Angel Veloz <vlzangel91@gmail.com>
2017-11-02 18:55:30 +0100

    Footer correo e iconos busqueda

--- wp-content/plugins/kmimos/kmimos-email.php
+++ wp-content/plugins/kmimos/kmimos-email.php
@@ -158,15 +158,15 @@ if(!function_exists('kmimos_get_email_footer')){
             $ayuda = "";
             if( $dudas ){
                 $ayuda = "
-                    <div style=\"float:left; width:600px; margin-bottom: 31px;\">   
-                        <div style=\"text-align:center;\">
-                            <p style=\"font-family: Arial; font-size:14px; color:#666; text-align: center; \">
-                                En caso de dudas, puedes contactarte con nuestro equipo de atención al cliente al teléfono (01) 55 4742 3162, Whatsapp <a href=\"telf:+525568922182\" target=\"_blank\" style=\"text-decoration: none;\">+52 (55) 6892 2182</a>, o al correo 
-                                <a href=\"mailto:contactomex@kmimos.la\" target=\"_blank\" style=\"text-decoration: none; \">contactomex@kmimos.la</a>
+                    <div style='float:left; width:600px; margin-bottom: 31px;'>   
+                        <div style='text-align:center;'>
+                            <p style='font-family: Arial; font-size:14px; color:#666; text-align: center; '>
+                                En caso de dudas, puedes contactarte con nuestro equipo de atención al cliente al teléfono (01) 55 4742 3162, Whatsapp <a href='telf:+525568922182' target='_blank' style='text-decoration: none;'>+52 (55) 6892 2182</a>, o al correo 
+                                <a href='mailto:contactomex@kmimos.la' target='_blank' style='text-decoration: none; '>contactomex@kmimos.la</a>
                             </p>
-                            <div  style=\"clear:both;\"></div>
+                            <div  style='clear:both;'></div>
                         </div>
-                        <div  style=\"clear:both;\"></div>
+                        <div  style='clear:both;'></div>
                     </div>
                 ";
             }
@@ -174,11 +174,11 @@ if(!function_exists('kmimos_get_email_footer')){
             $beneficios_txt = "";
             if( $beneficios ){
                 $beneficios_txt = "
-                    <div style=\"font-family: Arial; font-size: 12px; font-weight: bold; letter-spacing: 0.2px; color: #6b1c9b; margin-bottom: 10px;\">
+                    <div style='font-family: Arial; font-size: 12px; font-weight: bold; letter-spacing: 0.2px; color: #6b1c9b; margin-bottom: 10px;'>
                         CON LA CONFIANZA Y SEGURIDAD QUE NECESITAS
                     </div>
 
-                    <img style=\"margin-bottom: 16px;\" src=\"".get_home_url()."/wp-content/themes/kmimos/images/emails/caracteristicas.png\" >
+                    <img style='margin-bottom: 16px;' src='".get_home_url()."/wp-content/themes/kmimos/images/emails/caracteristicas.png' >
                 ";
             }
 
@@ -187,54 +187,54 @@ if(!function_exists('kmimos_get_email_footer')){
                 <head>
                     <style>
                         p{ margin:0px; }
-                        a[id*=\"kmimos_container\"] > div {
+                        a[id*='kmimos_container'] > div {
                             max-width: 600px !important;
                         }
                         @media max-device-width: 480px {
-                            a[id*=\"kmimos_container\"] > div {
+                            a[id*='kmimos_container'] > div {
                                 max-width: 600px !important;
                             }
                         }
                     </style>
                 </head>
                 <body>
-                    <div id=\"kmimos_container\" style=\"font-family: Arial;\">
-                        <div style=\"margin: 0px auto; padding: 0px 10px; width: 600px;\">
-                            <div style=\"text-align:center;\">
-                                <img src=\"".get_home_url()."/wp-content/themes/kmimos/images/emails/bitmap.png\" style=\"margin-bottom: 14px;\" />
+                    <div id='kmimos_container' style='font-family: Arial;'>
+                        <div style='margin: 0px auto; padding: 0px 10px; width: 600px;'>
+                            <div style='text-align:center;'>
+                                <img src='".get_home_url()."/wp-content/themes/kmimos/images/emails/bitmap.png' style='margin-bottom: 14px;' />
                             </div>
 
                             ".$content."
 
-                            <div style=\"text-align:center;\">
+                            <div style='text-align:center;'>
                                 
                                 ".$ayuda."
 
                                 ".$beneficios_txt."
 
-                                <img style=\"margin-bottom: 30px;\" src=\"".get_home_url()."/wp-content/themes/kmimos/images/emails/dog_footer.png\" >
+                                <img style='margin-bottom: 30px;' src='".get_home_url()."/wp-content/themes/kmimos/images/emails/dog_footer.png' >
 
-                                <div style=\"background-color:#000000; color: #fff; display: table; width: 100%; height: 62px; font-size: 11px; letter-spacing: 0.2px; padding: 0px; box-sizing: border-box;\">
+                                <div style='background-color:#000000; color: #fff; display: table; width: 100%; height: 62px; font-size: 11px; letter-spacing: 0.2px; padding: 0px; box-sizing: border-box;'>
 
-                                    <div style=\"display: table-cell; width: 240px; vertical-align: middle; text-align: left; padding-right: 15px; padding-left: 30px;\">
-                                        <a href=\"".get_home_url()."\" style=\"color: #FFF; text-decoration: none;\">
-                                            <img src=\"".get_home_url()."/wp-content/themes/kmimos/images/emails/kamimos_footer.png\" style=\"height: 21px;float:left;\"> 
+                                    <div style='display: table-cell; width: 240px; vertical-align: middle; text-align: left; padding-right: 15px; padding-left: 30px;'>
+                                        <a href='".get_home_url()."' style='color: #FFF; text-decoration: none;'>
+                                            <img src='".get_home_url()."/wp-content/themes/kmimos/images/emails/kamimos_footer.png' style='height: 21px;float:left;'> 
                                         </a>
                                     </div>
 
-                                    <div style=\"display: table-cell; width: 240px; vertical-align: middle; text-align: right; padding-left:15px; padding-right: 30px;\">
-                                        <span style=\"display: inline-block; padding: 0px 5px 2px 0px; float:right\">
+                                    <div style='display: table-cell; width: 240px; vertical-align: middle; text-align: right; padding-left:15px; padding-right: 30px;'>
+                                        <span style='display: inline-block; padding: 0px 5px 2px 0px; float:right'>
                                             Síguenos en 
-                                            <a href=\"https://www.facebook.com/Kmimosmx/\">
-                                                <img src=\"".get_home_url()."/wp-content/themes/kmimos/images/emails/icono_facebook.png\" style=\"vertical-align: bottom;\" align=\"center\">
+                                            <a href='https://www.facebook.com/Kmimosmx/'>
+                                                <img src='".get_home_url()."/wp-content/themes/kmimos/images/emails/icono_facebook.png' style='vertical-align: bottom;' align='center'>
                                             </a>
                                         </span> 
                                     </div>
 
                                 </div>
 
-                                <p style=\"text-align: center; font-family: Arial; font-size: 11px; line-height: 1.73; padding: 10px;\">
-                                    ¿Tienes dudas? | Contáctanos
+                                <p style='text-align: center; font-family: Arial; font-size: 11px; line-height: 1.73; padding: 10px;'>
+                                    ¿Tienes dudas? | <a href='".get_home_url()."/contacta-con-nosotros/'>Contáctanos</a>
                                 </p>
                             </div>
                         </div>      
