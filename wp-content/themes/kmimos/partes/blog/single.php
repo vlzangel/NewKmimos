5f73b3102f493c45cdc9cbdb8dd58ea93357d808
Italo <italococchini@gmail.com>
2017-11-06 13:00:26 -0400

    Blog - kmimos

--- wp-content/themes/kmimos/partes/blog/single.php
+++ wp-content/themes/kmimos/partes/blog/single.php
@@ -199,56 +199,60 @@ while(have_posts()){
 </section>
 
 <script type="text/javascript">
-    jQuery(document).on('click','#featured .show .icon', function(e){
-        featured_show(this);
-    });
+    $(function(jQuery){
 
-    function featured_show(element){
-        var featured = jQuery(element).closest('#featured');
-        if(featured.hasClass('show')){
-            featured.removeClass('show');
-            jQuery('#featured').css({'right':''});
-        }else{
-            featured.addClass('show');
-            var width= jQuery('#featured.show').width();
-            jQuery('#featured.show').css({'right':'calc(100% - '+width+'px)'});
-        }
-    }
 
-    function divide_content(column, separator){
-        var content = jQuery('#single .single .content');
-        var sections = content.children(separator).length;
-        var text = content.html();
-        content.html('');
+        jQuery(document).on('click','#featured .show .icon', function(e){
+            featured_show(this);
+        });
 
-        for(var i=0; i<column; i++){
-            content.append('<div class="column column'+i+'">'+text+'</div>');
-            text='';
-
-            if(i>0){
-                var iprev =  (i-1);
-                var divide = Math.ceil(sections/(column-iprev));
-                content.find('.column'+iprev).children(separator).slice(divide).appendTo(content.find('.column'+i));
+        function featured_show(element){
+            var featured = jQuery(element).closest('#featured');
+            if(featured.hasClass('show')){
+                featured.removeClass('show');
+                jQuery('#featured').css({'right':''});
+            }else{
+                featured.addClass('show');
+                var width= jQuery('#featured.show').width();
+                jQuery('#featured.show').css({'right':'calc(100% - '+width+'px)'});
             }
         }
-    }
-    divide_content(4, 'p, h1, h2, h3, ul');
 
+        function divide_content(column, separator){
+            var content = jQuery('#single .single .content');
+            var sections = content.children(separator).length;
+            var text = content.html();
+            content.html('');
 
+            for(var i=0; i<column; i++){
+                content.append('<div class="column column'+i+'">'+text+'</div>');
+                text='';
 
-
-    function insert_element(element, after,  separator){
-        var content = jQuery('#single .single .content');
-        var sections = content.find('.column').length;
-        var text = content.html();
-
-        if(sections>0){
-            content.find('.column').eq(after).after(jQuery(element).addClass('central'));
+                if(i>0){
+                    var iprev =  (i-1);
+                    var divide = Math.ceil(sections/(column-iprev));
+                    content.find('.column'+iprev).children(separator).slice(divide).appendTo(content.find('.column'+i));
+                }
+            }
         }
-    }
+        divide_content(4, 'p, h1, h2, h3, ul');
 
-    jQuery(document).ready(function(e){
-        insert_element('#featured', 0, 'p');
+
+
+
+        function insert_element(element, after,  separator){
+            var content = jQuery('#single .single .content');
+            var sections = content.find('.column').length;
+            var text = content.html();
+
+            if(sections>0){
+                content.find('.column').eq(after).after(jQuery(element).addClass('central'));
+            }
+        }
+
+        jQuery(document).ready(function(e){
+            insert_element('#featured', 0, 'p');
+        });
     });
 </script>
 
