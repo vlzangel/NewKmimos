16f6ca8d55fcab90e69caa782450da361a562484
Angel Veloz <vlzangel91@gmail.com>
2017-11-07 17:49:32 +0100

    Nuevo correo admin - Conocer Cuidador

--- wp-content/themes/kmimos/procesos/conocer/nueva.php
+++ wp-content/themes/kmimos/procesos/conocer/nueva.php
@@ -202,7 +202,7 @@
 
 		$info = kmimos_get_info_syte();
 
-		$cuidador_file = realpath('../../template/mail/conocer/cuidador.php');
+		$cuidador_file = realpath('../../template/mail/conocer/cuidador/nueva.php');
         $mensaje_cuidador = file_get_contents($cuidador_file);
 
         $fin = strtotime( str_replace("/", "-", $_POST['service_end']) );
@@ -235,7 +235,7 @@
 		Cliente
 	*/
 
-		$cliente_file = realpath('../../template/mail/conocer/cliente.php');
+		$cliente_file = realpath('../../template/mail/conocer/cliente/nueva.php');
         $mensaje_cliente = file_get_contents($cliente_file);
 
         $mensaje_cliente = str_replace('[name]', $cliente_web, $mensaje_cliente);
@@ -257,10 +257,41 @@
 		wp_mail( $email_cliente, $asunto, $mensaje_cliente);
 
 	/*
-		Enviando E-mails
+		Enviando E-mails Administradores
 	*/
 
-		kmimos_mails_administradores_new($asunto, $mensaje_cuidador);
+		$admin_file = realpath('../../template/mail/conocer/admin/nueva.php');
+        $mensaje_admin = file_get_contents($admin_file);
+
+        /* Generales */
+
+	        $mensaje_admin = str_replace('[URL_IMGS]', get_home_url()."/wp-content/themes/kmimos/images/emails", $mensaje_admin);
+	        $mensaje_admin = str_replace('[id_solicitud]', $request_id, $mensaje_admin);
+	        $mensaje_admin = str_replace('[fecha]', $_POST['meeting_when'], $mensaje_admin);
+	        $mensaje_admin = str_replace('[hora]', $_POST['meeting_time'], $mensaje_admin);
+	        $mensaje_admin = str_replace('[lugar]', $_POST['meeting_where'], $mensaje_admin);
+	        $mensaje_admin = str_replace('[desde]', date("d/m", strtotime( str_replace("/", "-", $_POST['service_start']) )), $mensaje_admin);
+	        $mensaje_admin = str_replace('[hasta]', date("d/m", $fin), $mensaje_admin);
+	        $mensaje_admin = str_replace('[anio]', date("Y", $fin), $mensaje_admin);
+       		$mensaje_admin = str_replace('[MASCOTAS]', $detalles_mascotas, $mensaje_admin);
+
+        /* Cliente */
+
+	        $mensaje_admin = str_replace('[nombre_cliente]', $cliente_web, $mensaje_admin);
+	        $mensaje_admin = str_replace('[avatar_cliente]', kmimos_get_foto($user_id), $mensaje_admin);
+	        $mensaje_admin = str_replace('[telefonos_cliente]', $telf_cliente, $mensaje_admin);
+	        $mensaje_admin = str_replace('[email_cliente]', $email_cliente, $mensaje_admin);
+
+        /* Cuidador */
+        
+	        $mensaje_admin = str_replace('[avatar_cuidador]', kmimos_get_foto($cuidador->user_id), $mensaje_admin);
+	        $mensaje_admin = str_replace('[nombre_cuidador]', $nombre_cuidador, $mensaje_admin);
+	        $mensaje_admin = str_replace('[telefonos_cuidador]', $telf_cuidador, $mensaje_admin);
+	        $mensaje_admin = str_replace('[email_cuidador]', $email_cuidador, $mensaje_admin);
+
+		$mensaje_admin = get_email_html($mensaje_admin, false);
+
+		kmimos_mails_administradores_new($asunto, $mensaje_admin);
 
 		$data = array(
 			'n_solicitud' => $request_id,
