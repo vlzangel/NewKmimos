699c894efbdb97de6eb8b5c3afcf9c2e20c00e7d
Angel Veloz <vlzangel91@gmail.com>
2017-11-24 18:39:09 +0100

    Act

---
+++ wp-content/themes/kmimos/admin/backend/fotos/ajax/moderar.php
@@ -0,0 +1,8 @@
+<?php
+    include_once(dirname(dirname(dirname(__DIR__)))."/recursos/conexion.php");
+
+	echo "<pre>";
+    	print_r($_POST);
+    echo "</pre>";
+
+?>
--- wp-content/themes/kmimos/admin/backend/fotos/modales/historial.php
+++ wp-content/themes/kmimos/admin/backend/fotos/modales/historial.php
@@ -6,21 +6,32 @@
     echo "</pre>";
 */
     $actual = time();
-    
+    $actual = strtotime("15:02:00");
+
 	$historial = "";
 	$fehas = $db->get_results("SELECT * FROM fotos WHERE reserva = {$ID}");
 	foreach ($fehas as $key => $value) {
 
         $status_val = $value->subio_12+$value->subio_06;
 
-        if( date("H", $actual) < 12 && $status_val == 1 ){
-            $status_val++;
-        }
+        if( date("Y-m-d", $actual) == $value->fecha ){
+            if( date("H", $actual) < 18 && $status_val == 1 ){
+                $status_val = 4;
+            }
 
-        if( date("H", $actual) > 12 && $status_val == 0 ){
-            $status_val = 3;
+            if( date("H", $actual) > 11 && $status_val == 0 ){
+                $status_val = 3;
+            }
+        }else{
+            if( $actual > strtotime( $value->fecha ) ){
+                if( $status_val == 0 ){
+                    $status_val = 3;
+                }
+            }else{
+                $status_val = 5;
+            }
         }
-
+        
         $status = ""; $status_txt = "";
         switch ( $status_val ) {
             case '0':
@@ -28,16 +39,24 @@
                 $status_txt = "Por cargar fotos";
             break;
             case '1':
-                $status = "status-ok";
-                $status_txt = "Todo Bien";
-            break;
-            case '2':
                 $status = "status-medio";
                 $status_txt = "Solo cargo un flujo";
+            break;
+            case '2':
+                $status = "status-ok";
+                $status_txt = "Todo Bien";
             break;
             case '3':
                 $status = "status-mal";
                 $status_txt = "No ha cargado a la hora";
+            break;
+            case '4':
+                $status = "status-ok-medio";
+                $status_txt = "Por cargar el flujo de la tarde";
+            break;
+            case '5':
+                $status = "status-futuro";
+                $status_txt = "Cargas futuras";
             break;
             
             default:
@@ -51,7 +70,7 @@
 
 		$historial .= "
 			<tr>
-				<td>".date("d/m/Y", strtotime($value->fecha))."</td>
+				<td>".date("d/m/Y", strtotime($value->fecha))." ".$status_val."</td>
 				<td>{$dia}</td>
 				<td>{$noche}</td>
 				<td> <div class='status {$status}' title='{$status_txt}'>&nbsp;</div> </td>
@@ -59,6 +78,30 @@
 		";
 	}
 ?>
+
+<div class="legenda">
+    <table width="100%" cellspacing="2" cellpadding="2" >
+        <tr>
+            <th> <span>Status previo al &uacute;ltimo env&iacute;o</span> </th>
+            <th> <span>Status despu&eacute;s del &uacute;ltimo env&iacute;o</span> </th>
+            <th> <span>Status futuros env&iacute;os</span> </th>
+        </tr>
+        <tr>
+            <td> <div class='status-2 status-inicio'>&nbsp;</div> Por cargar fotos</td>
+            <td> <div class='status-2 status-ok'>&nbsp;</div> Todo Bien</td>
+            <td> <div class='status-2 status-futuro'>&nbsp;</div> Cargas futuras</td>
+        </tr>
+        <tr>
+            <td> <div class='status-2 status-ok-medio'>&nbsp;</div> Cargo las fotos de la ma&ntilde;ana</td>
+            <td> <div class='status-2 status-medio'>&nbsp;</div> Solo cargo un flujo</td>
+        </tr>
+        <tr>
+            <td> <div class='status-2 status-mal'>&nbsp;</div> No ha cargado a la hora</td>
+            <td> <div class='status-2 status-mal'>&nbsp;</div> No ha cargado a la hora</td>
+        </tr>
+    </table>
+</div>
+
 <table width="100%" cellspacing="0" cellpadding="0" class="tabla_horizontal">
 	<tr>
 		<th>Fecha</th>
