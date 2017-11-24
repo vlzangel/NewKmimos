f6b6505403d41148d0aa977807841b9016d9e560
Angel Veloz <vlzangel91@gmail.com>
2017-11-24 14:41:16 +0100

    Act reporte

--- wp-content/themes/kmimos/admin/backend/fotos/modales/cuidador.php
+++ wp-content/themes/kmimos/admin/backend/fotos/modales/cuidador.php
@@ -1,1 +1,32 @@
-Cuidador
+<?php
+    include_once(dirname(dirname(dirname(__DIR__)))."/recursos/conexion.php");
+	
+	$email = $db->get_var("SELECT user_email FROM wp_users WHERE ID = {$ID}");
+
+	$metas = kmimos_get_user_meta($ID);
+
+	$name = $db->get_var("SELECT post_title FROM wp_posts WHERE post_author = {$ID} AND post_type = 'petsitters' ");
+
+	$name_real = $metas["first_name"]." ".$metas["last_name"];
+
+	$telf = array();
+    $telf[] = $metas["user_mobile"];
+    $telf[] = $metas["user_phone"];
+
+    $telf = implode(" / ", $telf);
+
+?>
+<table width="100%" cellspacing="0" cellpadding="0" class="tabla_vertical">
+	<tr>
+		<th>Nombre</th>
+		<td><?php echo utf8_encode( $name ); ?> (<?php echo utf8_encode( $name_real ); ?>)</td>
+	</tr>
+	<tr>
+		<th>E-mail</th>
+		<td><?php echo $email; ?></td>
+	</tr>
+	<tr>
+		<th>Tel&eacute;fono</th>
+		<td><?php echo $telf; ?></td>
+	</tr>
+</table>
--- wp-content/themes/kmimos/admin/backend/fotos/modales/historial.php
+++ wp-content/themes/kmimos/admin/backend/fotos/modales/historial.php
@@ -1,1 +1,58 @@
-Hola historial
+<?php
+    include_once(dirname(dirname(dirname(__DIR__)))."/recursos/conexion.php");
+
+/*    echo "<pre>";
+    	print_r($_POST);
+    echo "</pre>";
+*/
+	$historial = "";
+	$fehas = $db->get_results("SELECT * FROM fotos WHERE reserva = {$ID}");
+	foreach ($fehas as $key => $value) {
+
+        $status = ""; $status_txt = "";
+        switch ( $value->status ) {
+            case '0':
+                $status = "status-inicio";
+                $status_txt = "Por cargar fotos";
+            break;
+            case '1':
+                $status = "status-ok";
+                $status_txt = "Todo Bien";
+            break;
+            case '2':
+                $status = "status-medio";
+                $status_txt = "Solo cargo un flujo";
+            break;
+            case '3':
+                $status = "status-mal";
+                $status_txt = "No ha cargado a la hora";
+            break;
+            
+            default:
+                $status = "status-ok";
+                $status_txt = "Todo Bien";
+            break;
+        }
+
+        $dia = "No"; if( $value->subio_12 == 1 ){ $dia = "Si"; }
+        $noche = "No"; if( $value->subio_06 == 1 ){ $noche = "Si"; }
+
+		$historial .= "
+			<tr>
+				<td>".date("d/m/Y", strtotime($value->fecha))."</td>
+				<td>{$dia}</td>
+				<td>{$noche}</td>
+				<td> <div class='status {$status}' title='{$status_txt}'>&nbsp;</div> </td>
+			</tr>
+		";
+	}
+?>
+<table width="100%" cellspacing="0" cellpadding="0" class="tabla_horizontal">
+	<tr>
+		<th>Fecha</th>
+		<th>12 m</th>
+		<th>06 pm</th>
+		<th>Status</th>
+	</tr>
+	<?php echo $historial; ?>
+</table>
