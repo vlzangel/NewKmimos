f31d32c9346481082849ab032bfaa6baa972c3ad
Angel Veloz <vlzangel91@gmail.com>
2017-11-27 15:55:50 +0100

    Ultimos ajustes fotos

---
+++ cron/fotos.php
@@ -0,0 +1,116 @@
+<?php
+
+    session_start();
+    
+    require('../wp-load.php');
+
+    date_default_timezone_set('America/Mexico_City');
+
+    global $wpdb;
+
+    $hora_actual = strtotime("now");
+    $xhora_actual = date("H", $hora_actual);
+
+    $periodo_sql = "subio_12 = '1' ";
+    $periodo = 1;
+    if( $xhora_actual == "18" ){
+        $periodo_sql = "subio_06 = '1' ";
+        $periodo = 2;
+    }
+
+    $hoy = date("Y-m-d");
+
+    $SQL = "SELECT * FROM fotos WHERE {$periodo_sql} AND bloqueo = 0 AND fecha = '{$hoy}'";
+    $fotos_a_enviar = $wpdb->get_results( $SQL );
+
+    $PATH = dirname(__DIR__)."/wp-content/uploads/fotos/";
+
+    foreach ($fotos_a_enviar as $key => $value) {
+        $cliente_id = $wpdb->get_var("SELECT meta_value FROM wp_postmeta WHERE post_id = {$value->reserva} AND meta_key = '_booking_customer_id' ");
+        $frecuencia = get_user_meta($cliente_id, "user_recibir_fotos", true);
+
+        $email_cliente = $wpdb->get_var("SELECT user_email FROM wp_users WHERE ID = {$cliente_id} ");
+
+        $metas = get_user_meta($cliente_id);
+        $nombre_cliente = $metas["first_name"][0]." ".$metas["last_name"][0];
+
+        $enviar = true;
+        $flujos = 1;
+
+        switch ( $frecuencia ) {
+            case 2: // Una vez al dia
+                if( $periodo == 1 ){
+                    $enviar = false;
+                }else{
+                    $flujos = 2;
+                }
+            break;
+            case 3: // Una vez cada 2 dias
+                
+                $inicio = strtotime( substr(get_post_meta($value->reserva, "_booking_start", true), 0, 8) );
+                $fin    = strtotime( substr(get_post_meta($value->reserva, "_booking_end", true), 0, 8) );
+
+                $flujos = 2;
+
+                if( $inicio == $fin && $periodo == 1 ){
+                    $enviar = false;
+                }elseif ( $inicio == $fin && $periodo == 2 ) {
+                    $enviar = true;
+                }else{
+                    $transcurrido = ( ( strtotime($hoy)-$inicio ) / 86400)+1; // Ej: 22/11 a 21/11 = 1+1 = 2
+                    $dias = ( ( $fin-$inicio ) / 86400)+1;
+                    if( $dias == 2 && $transcurrido == 2 && $periodo == 1 ){ // Duracion 2 dias - periodo de la mañana
+                        $enviar = false;
+                    }elseif ( $dias == 2 && $transcurrido == 2 && $periodo == 2 ) { // Duracion 2 dias - periodo de la tarde
+                        $enviar = true;
+                    }else{
+                        if( $transcurrido%2 == 0 && $periodo == 1 ){ // Duracion mayor a 2 dias - periodo de la mañana
+                            $enviar = false;
+                        }elseif ( $transcurrido%2 == 0 && $periodo == 2 ) {
+                            $enviar = true;
+                        }
+                    }
+                }
+
+            break;
+            case 4: // No enviar nunca
+                $enviar = false;
+            break;
+        }
+
+        if( $enviar ){
+
+            $fotos = dirname(__DIR__).'/wp-content/themes/kmimos/template/mail/fotos/fotos.php';
+            $fotos = file_get_contents($fotos);
+
+            $periodo_txt = "";
+            if( $flujos == 1 ){
+                $temp = "ma&ntilde;ana";
+                if( $periodo == 2 ){
+                    $temp = "tarde";
+                }
+                $periodo_txt = " por la ".$temp;
+                $collage = '<img src="'.get_home_url().'/wp-content/uploads/fotos/'.$value->reserva.'/'.date("Y-m-d").'_'.$periodo.'/collage.png" style="width: 600px;" />';
+            }else{
+                if( $periodo == 2 ){
+                    $periodo_txt = " de la ma&ntilde;ana y la tarde";
+                    $collage  = '<img src="'.get_home_url().'/wp-content/uploads/fotos/'.$value->reserva.'/'.date("Y-m-d").'_1/collage.png" style="width: 600px;" />';
+                    $collage .= '<img src="'.get_home_url().'/wp-content/uploads/fotos/'.$value->reserva.'/'.date("Y-m-d").'_2/collage.png" style="width: 600px;" />';
+                }
+            }
+
+            $fotos = str_replace('[FOTOS]', $collage, $fotos);
+            $fotos = str_replace('[CLIENTE]', $nombre_cliente, $fotos);
+            $fotos = str_replace('[PERIODO]', $periodo_txt, $fotos);
+            $fotos = str_replace('[FECHA]', date("d/m/Y"), $fotos);
+            $fotos = str_replace('[ID_RESERVA]', $value->reserva, $fotos);
+            $fotos = str_replace('[URL_IMGS]', get_home_url()."/wp-content/themes/kmimos/images/emails", $fotos);
+
+            $fotos = get_email_html($fotos);
+
+            wp_mail( $email_cliente, "Fotos de tus mascotas", $fotos);
+
+        }
+    }
+
+?>
--- wp-content/themes/kmimos/admin/backend/fotos/reporte_fotos.js
+++ wp-content/themes/kmimos/admin/backend/fotos/reporte_fotos.js
@@ -1,5 +1,6 @@
+var table = "";
 jQuery(document).ready(function() {
-    jQuery('#example').DataTable({
+    table = jQuery('#example').DataTable({
     	"language": {
 			"emptyTable":			"No hay datos disponibles en la tabla.",
 			"info":		   			"Del _START_ al _END_ de _TOTAL_ ",
@@ -44,46 +45,92 @@ function abrir_link(e){
 	});
 }
 
+var checkes = {};
 function moderar(){
 
-	var IMGS = ""; var CONT = 1;
-	var checkes = {};
-	jQuery("#form_moderar input").each(function(){
-		if( jQuery(this).prop('checked') ){
-        	checkes[ jQuery(this).attr("id") ] = jQuery(this).val();
-        	IMGS += jQuery( "<img src='"+jQuery(this).attr("data-url")+"' class='img' id='base_"+CONT+"' >" )[0].outerHTML;
-        	CONT++;
-		}
-    });
+	if( !jQuery("#Moderar").hasClass("disable") ){
 
-    jQuery("#base").html( IMGS );
+		jQuery("#Moderar").addClass("disable");
+		jQuery("#Moderar").val("Procesando...");
+		
+		var IMGS = ""; var CONT = 1;
+		jQuery("#form_moderar input").each(function(){
+			if( jQuery(this).prop('checked') ){
+	        	IMGS += jQuery( "<img src='"+jQuery(this).attr("data-url")+"' class='img' id='base_"+CONT+"' >" )[0].outerHTML;
+	        	CONT++;
+			}
+	    });
+	    jQuery("#base").html( IMGS );
 
-    checkes[ "ID_RESERVA" ] = ID_RESERVA;
-    checkes[ "CANTIDAD" ] = jQuery("#cantidad").val();
+	    var c = document.getElementById("myCanvas");
+	    var ctx = c.getContext("2d");
+	    var img = document.getElementById("fondo");
+	    ctx.drawImage(img, 0, 0, 600, 495);
 
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    var img = document.getElementById("fondo");
-    ctx.drawImage(img, 0, 0, 600, 495);
+	    jQuery( "#base img" ).each(function( index ) {
+	        var img = document.getElementById( jQuery(this).attr("id") );
+	        img.onload = function () {
+		        var i = jQuery(this).attr("id");
+		        ctx.drawImage(
+		            img, 
+		            jQuery( "#"+i )[0].offsetLeft, 
+		            jQuery( "#"+i )[0].offsetTop, 
+		            jQuery( "#"+i )[0].offsetWidth, 
+		            jQuery( "#"+i )[0].offsetHeight
+		        );     
+		    };	        
+	    });
 
-    jQuery( "#base img" ).each(function( index ) {
-        var img = document.getElementById( jQuery(this).attr("id") );
-        var i = jQuery(this).attr("id");
-        console.log(img);
-        ctx.drawImage(
-            img, 
-            jQuery( "#"+i )[0].offsetLeft, 
-            jQuery( "#"+i )[0].offsetTop, 
-            jQuery( "#"+i )[0].offsetWidth, 
-            jQuery( "#"+i )[0].offsetHeight
-        );
-    });
+	    setTimeout(function(){
 
-	jQuery.post(
-		TEMA+"/admin/backend/fotos/ajax/moderar.php",
-		checkes,
-		function(HTML){
-            console.log(HTML);
-        }
-    );
+	    	var checkes = {};
+			jQuery("#form_moderar input").each(function(){
+				if( jQuery(this).prop('checked') ){
+		        	checkes[ jQuery(this).attr("id") ] = jQuery(this).val();
+		        	CONT++;
+				}
+		    });
+		    checkes[ "ID_RESERVA" ] = ID_RESERVA;
+		    checkes[ "PERIODO" ] = PERIODO;
+		    checkes[ "COLLAGE" ] = jQuery( "#myCanvas" )[ 0 ].toDataURL("image/jpg");
+
+	        jQuery.post(
+				TEMA+"/admin/backend/fotos/ajax/moderar.php",
+				checkes,
+				function(HTML){
+
+					console.log( HTML );
+
+		            jQuery("#Moderar").removeClass("disable");
+		            jQuery("#Moderar").val("Moderar");
+
+		            table.ajax.reload();
+		        }
+		    );   
+	    }, 500);
+
+	}
+
+}
+
+function Bloquear(){
+	if( !jQuery("#Bloquear").hasClass("disable") ){
+
+		jQuery("#Bloquear").addClass("disable");
+		jQuery("#Bloquear").val("Procesando...");
+
+		jQuery.post(
+			TEMA+"/admin/backend/fotos/ajax/bloqueo.php",
+			{
+				bloquear: jQuery("#bloquear").val(),
+				ID_RESERVA: ID_RESERVA
+			},
+			function(HTML){
+	            jQuery("#Bloquear").removeClass("disable");
+	            jQuery("#Bloquear").val("Bloquear");
+
+	            table.ajax.reload();
+	        }
+	    ); 
+	}
 }
