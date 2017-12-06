c7e4f84c178dad01493b179b9e5087ae2de3e443
Dajan Medina <dmedina2150@gmail.com>
2017-12-06 18:57:31 -0400

    Agregando configuracion archivo peru.php

---
+++ wp-content/plugins/kmimos/regionalizacion/peru.php
@@ -0,0 +1,56 @@
+<?php  
+$region = array(
+
+    "estado"=> 'Ciudad',
+    "municipio"=>'',
+    'direccion'=> 'Localidad o barrio',
+    'precio_sugerido_pequeno' => '20 a 30',
+    'precio_sugerido_mediano' => '25 a 35',
+    'precio_sugerido_grande'  => '30 a 40',
+    'precio_sugerido_gigante' => '35 a 50',
+    'cuidador_descripcion' => '¡Hola! Soy ________, tengo ___ años y me encantan los animales. Estaré 100% al cuidado de tu perrito, lo consentiré y recibirás fotos diarias de su estancia conmigo. Mis huéspedes peludos duermen dentro de casa SIN JAULAS NI ENCERRADOS. Cuento con _______ para que jueguen, además cerca de casa hay varios parques donde los saco a pasear diariamente. En su estancia tu perrito contará con cobertura de gastos veterinarios, que en caso de emergencia se encuentra a dentro de mi colonia, muy cerca de mi casa. Cualquier duda que tengas no dudes en contactarme.',
+    "pais" => "Peru",
+    "pais_cod_iso"   => "PE",
+    'moneda_cod' => 'S./',
+    "mon_der" => "S./",
+    "titulo"    => "Kmimos Perú",
+    "email"     => "contactope@kmimos.la",
+
+    "telefono" => " Tlf:(+51) 963 585 100",
+    "telefono_sin" => " +51 963 585 100",
+    "telefono_atencion" => " ",
+
+    "telefono_corrido" => "+51963585100",
+    "whatsapp" => "(+51) 963 585 100", 
+    "twitter" => "kmimospe",
+    "facebook" => "Kmimospe",
+    "instagram" => "kmimospe",
+    "mon_izq" => "",
+    "km-vacaciones" => false,
+    "km-medios" => false,
+    "km-patitas" => false,
+    "km-5000" => 0,
+    "length_movil" => 9,
+    "conocio" => array(
+                'Facebook'      =>  'Facebook',
+                'Adwords'       =>  'Buscador de Google',
+                'Twitter'       =>  'Twitter',
+                'Amigo/Familiar'        =>  'Recomendación de amigo o familiar',
+                'Youtube'               =>  'Youtube',
+                'Otros'                 =>  'Otros'
+            ),
+    "identidad_corta" => "Cédula /Documento de identidad",
+    "identidad-length" => 20,
+    "identidad" => "Cédula de Ciudadanía /DNI",
+    "meta_key_pdf_tienda" => "_payu_pdf",
+    "meta_key_tienda_vence" => "_payu_tienda_vence",
+    "web" => "http://kmimos.pe",
+    "comision" => "",
+    "ubicaciones" => "UBICACI&Oacute;N, CIUDAD, LOCALIDAD, BARRIO",
+    "Ciudad" => "Ciudad",
+    "Localidad" => "Localidad",
+    "Barrio" => "Barrio",
+    "ubicacion_cuidador" => " ",
+);
+
+
