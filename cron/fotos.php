162a1031df5acbf9702c8fddb263d34a43d1fc3c
Angel Veloz <vlzangel91@gmail.com>
2017-12-20 15:31:44 +0100

    act

--- cron/fotos.php
+++ cron/fotos.php
@@ -94,10 +94,10 @@
                 $collage = "";
                 foreach ($moderacion[ $periodos_a_mostrar ] as $key => $foto) {
                     $collage .= '
-                    <div style="display: inline-block; width: 49%; text-align: center; min-width: 239px;">
+                    <div style="display: inline-block; width: 49%; text-align: center;">
                         <a style="display: block; margin: 3px 3px 8px; text-decoration: none;">
                             <div style="background: #f3f3f3; border: solid 1px #CCC; padding: 10px; border-radius: 4px;">
-                                <img src="'.get_home_url().'/wp-content/uploads/fotos/'.$value->reserva.'/'.date("Y-m-d").'_'.$periodo.'/'.$foto.'" height="130" style="max-width: 100%; max-height: 100%;" />
+                                <img src="'.get_home_url().'/wp-content/uploads/fotos/'.$value->reserva.'/'.date("Y-m-d").'_'.$periodo.'/'.$foto.'" style="height: 130px; max-width: 100%; max-height: 100%;" />
                             </div>
                         </a>
                     </div>';
@@ -111,10 +111,10 @@
                         if( count($moderacion[ 1 ]) > 0 ){
                             foreach ($moderacion[ 1 ] as $key => $foto) {
                                 $collage .= '
-                                <div style="display: inline-block; width: 49%; text-align: center; min-width: 239px;">
+                                <div style="display: inline-block; width: 49%; text-align: center;">
                                     <a style="display: block; margin: 3px 3px 8px; text-decoration: none;">
                                         <div style="background: #f3f3f3; border: solid 1px #CCC; padding: 10px; border-radius: 4px;">
-                                            <img src="'.get_home_url().'/wp-content/uploads/fotos/'.$value->reserva.'/'.date("Y-m-d").'_1/'.$foto.'" height="130" style="max-width: 100%; max-height: 100%;" />
+                                            <img src="'.get_home_url().'/wp-content/uploads/fotos/'.$value->reserva.'/'.date("Y-m-d").'_1/'.$foto.'" style="height: 130px; max-width: 100%; max-height: 100%;" />
                                         </div>
                                     </a>
                                 </div>';
