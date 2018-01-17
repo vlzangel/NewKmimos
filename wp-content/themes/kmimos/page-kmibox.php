fa31bb8d93eb0ed185923f7acfda151887428861
Angel Veloz <vlzangel91@gmail.com>
2018-01-17 18:00:40 +0100

    Kmibox (Proximamente) y eliminando los parentesis de los correos

---
+++ wp-content/themes/kmimos/page-kmibox.php
@@ -0,0 +1,16 @@
+<?php 
+    /*
+        Template Name: Kmibox
+    */
+	
+	get_header();
+
+	echo '
+ 		<div class="km-ficha-bg" style="background-image:url('.getTema().'/images/new/km-ficha/km-bg-ficha.jpg);">
+			<div class="overlay"></div>
+		</div>
+		<h1 style="text-align: center;">Próximamente</h1>
+	';
+
+	get_footer();
+?>
