989a69ff1eb11b9d5eaf9eb18c8b6f6ed31050ef
Angel Veloz <vlzangel91@gmail.com>
2018-01-25 14:27:59 +0100

    Valoraciones - Agregando moderacion

--- wp-content/themes/kmimos/procesos/perfil/valorar.php
+++ wp-content/themes/kmimos/procesos/perfil/valorar.php
@@ -1,5 +1,5 @@
 <?php
-
+	
     date_default_timezone_set('America/Mexico_City');
     $hoy = date("Y-m-d H:i:s");
 
@@ -21,7 +21,7 @@
 	    	'{$hoy}', 
 	    	'{$comentarios}', 
 	    	'0', 
-	    	'1', 
+	    	'0', 
 	    	'', 
 	    	'', 
 	    	'0', 
@@ -40,6 +40,50 @@
 	$sql .= "INSERT INTO wp_postmeta VALUES (NULL, '{$post_id}', 'customer_comment', '{$coment_id}'); ";
 
 	$db->query_multiple( utf8_decode($sql) );
+
+	$HTML = '
+		<div style="margin: 0px auto; width: 600px;font-size: 17px;">
+			<div style="margin-bottom: 10px;">
+				Una nueva valoraci&oacute;n para el cuidador <a style="text-decoration: none;" href="[LINK_CUIDADOR]">"[CUIDADOR]"</a> está esperando tu aprobación.
+			</div> 
+
+			<div style="font-weight: 600; margin-bottom: 10px;">
+				Valoraci&oacute;n: 
+			</div> 
+
+			<div style="margin-bottom: 25px;">
+				[COMENTARIO]
+			</div> 
+
+			<div style="text-align: center;">
+				<a style="display: inline-block; padding: 10px; text-decoration: none; border: solid 1px #CCC; padding: 5px 10px; font-size: 13px;" href="'.get_home_url().'/wp-admin/comment.php?action=approve&c='.$coment_id.'">
+					Aprobarlo
+				</a>
+				<a style="display: inline-block; padding: 10px; text-decoration: none; border: solid 1px #CCC; padding: 5px 10px; font-size: 13px;" href="'.get_home_url().'/wp-admin/comment.php?action=trash&c='.$coment_id.'">
+				Enviar a la papelera
+				</a>
+				<a style="display: inline-block; padding: 10px; text-decoration: none; border: solid 1px #CCC; padding: 5px 10px; font-size: 13px;" href="'.get_home_url().'/wp-admin/comment.php?action=spam&c='.$coment_id.'">
+					Marcarlo como spam
+				</a>
+				<a style="display: inline-block; padding: 10px; text-decoration: none; border: solid 1px #CCC; padding: 5px 10px; font-size: 13px;" href="'.get_home_url().'/wp-admin/edit-comments.php?comment_status=moderated">
+					M&aacute;s Moderaciones
+				</a>
+			</div> 
+			
+		</div> 
+	';
+
+	$comentario = $comentarios;
+
+	$cuidador = $db->get_row("SELECT * FROM wp_posts WHERE ID = {$post_id}");
+
+	$HTML = str_replace("[CUIDADOR]", $cuidador->post_title, $HTML);
+	$HTML = str_replace("[COMENTARIO]", $comentario, $HTML);
+	$HTML = str_replace("[LINK_CUIDADOR]", get_home_url()."/petsitters/".$cuidador->post_name, $HTML);
+
+	include( dirname(__DIR__, 5)."/wp-load.php" );
+
+	wp_mail("contactomex@kmimos.la", "Nueva Valoraci&oacute;n para: ".$cuidador->post_title, $HTML);
 
 	$respuesta = array(
 		"status" => "OK"
