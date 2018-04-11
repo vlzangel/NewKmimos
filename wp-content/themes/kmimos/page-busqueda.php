a7cbc9d12a18dc0c636f77807456ee528bab7b20
Angel Veloz <vlzangel91@gmail.com>
2018-04-11 16:58:56 +0200

    Ajuste - cargando - buscando

--- wp-content/themes/kmimos/page-busqueda.php
+++ wp-content/themes/kmimos/page-busqueda.php
@@ -153,7 +153,7 @@
 
 		<div class="container contentenedor-buscador-todos content-wlabel-search">
 			<div class="km-contentido-formulario-buscador">
-				<form class="km-formulario-buscador" action="'.get_home_url().'/wp-content/themes/kmimos/procesos/busqueda/buscar.php" method="post">
+				<form onSubmit="jQuery(\'#buscando\').css(\'display\', \'block\');" class="km-formulario-buscador" action="'.get_home_url().'/wp-content/themes/kmimos/procesos/busqueda/buscar.php" method="post">
 					<div class="km-bloque-cajas km-search-wlabel" >
 						<div class="km-div-ubicacion">
 						
@@ -182,10 +182,11 @@
 							<input type="text" id="checkin" name="checkin" placeholder="DESDE" value="'.$busqueda["checkin"].'" class="km-input-custom km-input-date date_from" readonly>
 							<input type="text" id="checkout" name="checkout" placeholder="HASTA" value="'.$busqueda["checkout"].'" class="km-input-custom km-input-date date_to" readonly>
 						</div>
-						<div class="km-div-enviar">
+						<div class="km-div-enviar" style="position: relative;">
 							<button type="submit" class="km-submit-custom" name="button">
 								BUSCAR
 							</button>
+							<i id="buscando" class="fa fa-spinner fa-spin" style="position: absolute; top: 10px; right: 10px; font-size: 24px; color: #FFF; display: none;"></i>
 						</div>
 						<div class="clear"></div>
 					</div>
