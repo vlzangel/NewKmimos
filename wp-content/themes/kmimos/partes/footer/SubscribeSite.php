302ef9fb464b22ef345a3a9cbdb4c0ce2912b7d8
Ángel Veloz <vlzangel91@gmail.com>
2018-05-24 23:30:45 -0430

    Banner modale home petco

--- wp-content/themes/kmimos/partes/footer/SubscribeSite.php
+++ wp-content/themes/kmimos/partes/footer/SubscribeSite.php
@@ -27,37 +27,56 @@
 		    }
 
 		    jQuery(element).find('.contain').html(html);
-		    jQuery(element).fadeIn(500,function(){
-		        /*
-		         vsetTime = setTimeout(function(){
-		         SubscribePopUp_Close(element);
-		        }, 6000);
-		        */
+		    jQuery(element).fadeIn(5,function(){ /* 500 */
 		    });
 		}
-
-	    function SubscribeSite(){
-	        clearTimeout(SubscribeTime);
-
-	        var dog = '<img height=\"70\" align=\"bottom\" src=\"https://www.kmimos.com.mx/wp-content/uploads/2017/07/propuestas-banner-09.png\">' +
-	            '<img height=\"20\" align=\"bottom\" src=\"https://www.kmimos.com.mx/wp-content/uploads/2017/07/propuestas-banner-10.png\">';
-
-	        var html='<div id=\"PageSubscribe\"><i class=\"exit fa fa-times\" aria-hidden=\"true\" onclick=\"SubscribePopUp_Close(\'#message.Msubscribe\')\"></i>' +
-	            '<div class=\"section section1\"><span>G&aacute;nate <strong>$50</strong> pesos en tu primera reserva</span><br>&#8216;&#8216;Aplica para clientes nuevos&#8217;&#8217;<div class=\"images\">'+dog+'</div></div>' +
-	            '<div class=\"section section2\"><span><strong>&#161;SUSCR&Iacute;BETE!</strong> y recibe el Newsletter con nuestras <strong>PROMOCIONES, TIPS DE CUIDADOS PARA MASCOTAS,</strong> etc.!</span>". subscribe_input($PopUpSection)." </div>' +
-	            '</div>';
-
-	        SubscribePopUp_Create(html);
-	    }
 
 		jQuery(document).ready(function(e){
 	        var body= jQuery('body');
 	        if((body.hasClass('home') && checkparam) || (body.hasClass('page-template-blog') || body.hasClass('single-post'))){
 	            SubscribeTime = setTimeout(function(){
 	                SubscribeSite();
-	            }, 7400);
+	            }, 10); /* 7400 */
 	        }
-	    });
-        
-	</script>
-	";
+	    });";
+
+	   	if( $PopUpSection == "petco" ){
+	   		$HTML .= "
+	   			function SubscribeSite(){
+			        clearTimeout(SubscribeTime);
+
+			        var dog = '';
+
+			        var html = 
+			        '<div id=\"PageSubscribe\" style=\"padding: 0px;\">'+
+			        '	<img src=\"".getTema()."/images/new/banner_petco_2.png\" class=\"banner_home_petco\" >'+
+			        '	<div class=\"banner_home_petco_components\"> '+
+			        ' 		<i class=\"exit fa fa-times\" aria-hidden=\"true\" onclick=\"SubscribePopUp_Close(\'#message.Msubscribe\')\"></i>' +
+			        '		<div class=\"section section1\">'+
+			        '			<div class=\"section section2\">". subscribe_input($PopUpSection)."</div>'+
+			        '		</div>' +
+			        '	</div>' +
+			        '</div>';
+
+			        SubscribePopUp_Create(html);
+			    }
+	   		";
+	   	}else{
+	   		$HTML .= "
+	   			function SubscribeSite(){
+			        clearTimeout(SubscribeTime);
+
+			        var dog = '<img height=\"70\" align=\"bottom\" src=\"https://www.kmimos.com.mx/wp-content/uploads/2017/07/propuestas-banner-09.png\">' +
+			            '<img height=\"20\" align=\"bottom\" src=\"https://www.kmimos.com.mx/wp-content/uploads/2017/07/propuestas-banner-10.png\">';
+
+			        var html='<div id=\"PageSubscribe\"><i class=\"exit fa fa-times\" aria-hidden=\"true\" onclick=\"SubscribePopUp_Close(\'#message.Msubscribe\')\"></i>' +
+			            '<div class=\"section section1\"><span>G&aacute;nate <strong>$50</strong> pesos en tu primera reserva</span><br>&#8216;&#8216;Aplica para clientes nuevos&#8217;&#8217;<div class=\"images\">'+dog+'</div></div>' +
+			            '<div class=\"section section2\"><span><strong>&#161;SUSCR&Iacute;BETE!</strong> y recibe el Newsletter con nuestras <strong>PROMOCIONES, TIPS DE CUIDADOS PARA MASCOTAS,</strong> etc.!</span>". subscribe_input($PopUpSection)." </div>' +
+			            '</div>';
+
+			        SubscribePopUp_Create(html);
+			    }
+	   		";
+	   	} $HTML .= "
+	</script>";
+?>
