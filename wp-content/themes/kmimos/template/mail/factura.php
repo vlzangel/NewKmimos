5a4ab7533128b36cff84811ebafd697767063b2d
Italo <italococchini@gmail.com>
2018-07-13 02:45:41 -0400

    formato email factura

---
+++ wp-content/themes/kmimos/template/mail/factura.php
@@ -0,0 +1,27 @@
+<div style='text-align:center; margin-bottom: 34px;'>
+	<img src='[URL_IMGS]/header_factura.png' style='width: 100%;' >
+</div>
+
+<div style='padding: 0px;'>
+
+	<div style='font-size: 14px; line-height: 1.07; letter-spacing: 0.3px; color: #000000;'>
+        
+		<div style='font-family: Arial; font-size: 20px; font-weight: bold; letter-spacing: 0.4px; color: #6b1c9b; padding-bottom: 10px;'>
+			Hola [name]
+		</div>	
+
+		<div style='font-family: Arial; font-size: 14px; line-height: 1.07; letter-spacing: 0.3px; color: #000000; padding-bottom: 25px; text-align: center;'>
+            Recibimos tu solicitud de Comprobante Fiscal Digital #[serie_folio], a continuación encontrarás el enlace para descargar tu CFDI.
+        </div>
+
+	     <div style='display: block; margin-bottom: 8px; text-align: center;'>
+	        <a href='[PDF]' target="_blank"><img src='[URL_IMGS]/group-5.png' style='' ></a>
+	    </div>
+	    
+	    <div style='font-family: Arial; font-size: 14px; letter-spacing: 0.3px; color: #2d2d2d; text-align: center; margin-bottom: 30px;'>
+	        (Dar click para descargar CFDI)
+	    </div>
+
+	</div>
+
+</div>
