98639879e1daa5e65e5a04baf68ac1f00cd9f300
Ángel Veloz <vlzangel91@gmail.com>
2018-07-12 21:36:15 -0430

    Datos de conocer en confirmación

--- wp-content/themes/kmimos/template/mail/conocer/cliente/confirmar.php
+++ wp-content/themes/kmimos/template/mail/conocer/cliente/confirmar.php
@@ -4,15 +4,81 @@
 
 <div style='padding: 0px; margin-bottom: 34px;'>
 
-	<div style='margin-bottom: 15px; font-size: 14px; line-height: 1.07; letter-spacing: 0.3px; color: #000000;'>
+	<div style='margin-bottom: 15px; font-size: 14px; line-height: 1.07; letter-spacing: 0.3px; color: #000000; padding-bottom: 0px;'>
 		<div style='font-family: Arial; font-size: 20px; font-weight: bold; letter-spacing: 0.4px; color: #6b1c9b; padding-bottom: 10px; text-align: center;'>
 			<img style='' src='[URL_IMGS]/check.png' >
 		</div>	<div style='font-family: Arial; font-size: 20px; font-weight: bold; letter-spacing: 0.4px; color: #6b1c9b; padding-bottom: 10px; text-align: center;'>
-			¡Todo está listo [name_cliente]!
+			¡Todo est&aacute; listo [name_cliente]!
 		</div>	
-	    <div style='font-family: Arial; font-size: 14px; line-height: 1.07; letter-spacing: 0.3px; color: #000000; padding-bottom: 0px; text-align: center;'>
+	    <div style='font-family: Arial; font-size: 14px; line-height: 1.07; letter-spacing: 0.3px; color: #000000; padding-bottom: 35px; text-align: center;'>
 	    	Tu solicitud para conocer al cuidador <strong>[name_cuidador]</strong> ha sido confirmada. <img style='' src='[URL_IMGS]/emoji_g.png' >
 	    </div>
+
+		<table cellpadding="0" cellspacing="0" style='margin-bottom: 40px; width: 600px;'>
+			<tr>
+				<td style='width: 62px; padding-right: 20px;'>
+					<img src='[avatar]' style='width: 62px; height: 62px; border-radius: 50%;' >
+				</td>
+				<td style='vertical-align: middle; padding-left: 16px; width: 326px;'>
+					<div style='font-family: Arial; font-size: 11px; font-weight: bold; letter-spacing: -0.1px; color: #0d7ad9;'>
+						DATOS DEL CUIDADOR 
+					</div>				    
+				    <div style='font-family: Arial; font-size: 20px; font-weight: bold; letter-spacing: 0.4px; color: #000000; margin-bottom: 2px;'>
+				    	[nombre_usuario]
+				    </div>			    
+				    <div style='font-family: Arial; font-size: 14px; letter-spacing: 0.3px; color: #000000; margin-bottom: 2px;'>
+				    	[telefonos]
+				    </div>	  
+				    <div style='font-family: Arial; font-size: 14px; letter-spacing: 0.3px; color: #000000; margin-bottom: 12px;'>
+				    	<a href="mailto:[email]">[email]</a>
+				    </div>
+				</td>
+				<td style='width: 159px; height: 50.3px; border-radius: 2.8px;  vertical-align: top;'>
+					<div style='background-color: #f4f4f4; height: 50.3px;'>
+						<div style='font-family: Arial; font-size: 12px; letter-spacing: 0.3px; color: #000000; padding: 10px 0px 0px 10px;'>
+							Tu c&oacute;digo de solicitud es:
+						</div>
+						<div style='font-family: Arial; font-size: 14px; font-weight: bold; letter-spacing: 0px; color: #000000; padding: 3px 0px 0px 10px;'>
+							[id_solicitud]
+						</div>
+					</div>
+				</td>
+			</tr>
+		</table>
+
+		<table cellpadding="0" cellspacing="0" style='margin-bottom: 31px; width: 600px;'>
+			<tr>
+				<td style='width: 62px; padding-right: 20px;'>
+					<img src='[URL_IMGS]/calendar.png' style='width: 77px; height: 70px;' >
+				</td>
+				<td style='vertical-align: top; padding-left: 0px; width: 242.5px;'>
+					<div style='font-family: Arial; font-size: 11px; font-weight: bold; letter-spacing: -0.1px; color: #0d7ad9;'>
+						DATOS DE LA REUNIÓN 
+					</div>				    
+				    <div style='font-family: Arial; font-size: 15px; font-weight: bold; letter-spacing: 0.4px; color: #000000; margin-bottom: 2px;'>
+				    	[fecha]
+				    </div>			    
+				    <div style='font-family: Arial; font-size: 14px; letter-spacing: 0.3px; color: #000000; margin-bottom: 2px;'>
+				    	[hora] horas
+				    </div>		    
+				    <div style='font-family: Arial; font-size: 14px; letter-spacing: 0.3px; color: #000000; margin-bottom: 12px;'>
+				    	[lugar]
+				    </div>
+				</td>
+				<td style='vertical-align: top; padding-left: 16px; width: 226.5px;'>
+					<div style='font-family: Arial; font-size: 11px; font-weight: bold; letter-spacing: -0.1px; color: #0d7ad9;'>
+						POSIBLE FECHA DE ESTADÍA 
+					</div>				    
+				    <div style='font-family: Arial; font-size: 15px; font-weight: bold; letter-spacing: 0.4px; color: #000000; margin-bottom: 2px;'>
+				    	Del [desde] al [hasta]
+				    </div>			    
+				    <div style='font-family: Arial; font-size: 14px; letter-spacing: 0.3px; color: #000000; margin-bottom: 12px;'>
+				    	del [anio]
+				    </div>
+				</td>
+			</tr>
+		</table>
+
 	</div>
 	
 </div>
