aaa0b6eef56f221a00d0c21606222c9f731e47a4
Ángel Veloz <vlzangel91@gmail.com>
2018-07-16 21:58:34 -0430

    Ajustes cambio de status

--- wp-content/themes/kmimos/admin/backend/otros/ajax/updateStatus.php
+++ wp-content/themes/kmimos/admin/backend/otros/ajax/updateStatus.php
@@ -4,13 +4,34 @@
     $raiz = dirname(dirname(dirname(dirname(dirname(dirname(dirname(__DIR__)))))));
     include_once($raiz."/wp-load.php");
 
+    global $wpdb;
+
     $superAdmin = "YES";
 
     $id_orden = $ORDEN_ID;
 
+    $orden_status = $wpdb->get_var("SELECT post_status FROM wp_posts WHERE ID = {$id_orden}");
+    $reserva = $wpdb->get_row("SELECT * FROM wp_posts WHERE post_parent = {$id_orden} AND post_type LIKE 'wc_booking'");
+    $reserva_status = $reserva->post_status;
+
+    $cliente_id = $reserva->post_author;
+
+    $cliente_email = $wpdb->get_var("SELECT user_email FROM wp_users WHERE ID = {$cliente_id}");
+    $metas_cliente = get_user_meta($cliente_id);
+
+    $producto = $wpdb->get_row("SELECT * FROM $wpdb->posts WHERE ID = '".get_post_meta($reserva->ID, '_booking_product_id', true)."'");
+
+    $cuidador_email = $wpdb->get_var("SELECT user_email FROM wp_users WHERE ID = {$producto->post_author}");
+    $metas_cuidador = get_user_meta($producto->post_author);
+
+    $new_status = "";
+
     switch ( $status ) {
         
         case 'pagado':
+
+            $new_status = "Pagado";
+
             $id_reserva = $wpdb->get_var("SELECT ID FROM wp_posts WHERE post_parent = {$id_orden} AND post_type LIKE 'wc_booking'");
 
             $id_item = $wpdb->get_var("SELECT meta_value FROM wp_postmeta WHERE post_id = {$id_reserva} AND meta_key = '_booking_order_item_id' ");
@@ -33,6 +54,8 @@
         break;
 
         case 'pagado_email':
+
+            $new_status = "Pagado con env&iacute;o de correo";
 
             $id_reserva = $wpdb->get_var("SELECT ID FROM wp_posts WHERE post_parent = {$id_orden} AND post_type LIKE 'wc_booking'");
 
@@ -58,6 +81,8 @@
         break;
 
         case 'confirmado':
+
+            $new_status = "Confirmado";
             
             $acc = "CFM"; $usu = "CUI"; $NO_ENVIAR = "NO";
 
@@ -66,6 +91,8 @@
         break;
 
         case 'confirmado_email':
+
+            $new_status = "Confirmado con env&iacute;o de correo";
             
             $acc = "CFM"; $usu = "CUI";
 
@@ -74,6 +101,8 @@
         break;
 
         case 'cancelado':
+
+            $new_status = "Cancelado";
             
             $acc = "CCL"; $usu = "CUI"; $NO_ENVIAR = "NO";
 
@@ -82,6 +111,8 @@
         break;
 
         case 'cancelado_email':
+
+            $new_status = "Cancelado con env&iacute;o de correo";
             
             $acc = "CCL"; $usu = "CUI";
 
@@ -89,6 +120,58 @@
 
         break;
     
+    }
+
+    function getStatusTxt($reserva, $orden){
+        switch ( $reserva."-".$orden ) {
+
+            case "wc-confirmed-confirmed":
+                return "Confirmado";
+            break;
+
+            case "modified-modified":
+                return "Modificado";
+            break;
+
+            case "wc-cancelled-cancelled":
+                return "Cancelado";
+            break;
+
+            case "paid-wc-completed":
+                return "Pendiente por Confirmar";
+            break;
+            
+            default:
+                return "Otro Status: ( ".$reserva."-".$orden." )";
+            break;
+
+        }
+    }
+
+    $status_actual = getStatusTxt( $orden_status, $reserva_status );
+
+
+    if( $new_status != "" ){
+        $file = $PATH_TEMPLATE.'/template/mail/status/admin.php';
+        $mensaje = file_get_contents($file);
+
+        $mensaje = str_replace('[RESERVA]', $reserva->ID, $mensaje);
+
+        $mensaje = str_replace('[CLIENTE]', $metas_cliente["first_name"][0]." ".$metas_cliente["last_name"][0], $mensaje);
+        $mensaje = str_replace('[CLIENTE_EMAIL]', $cliente_email, $mensaje);
+
+        $mensaje = str_replace('[CUIDADOR]', $metas_cuidador["first_name"][0]." ".$metas_cuidador["last_name"][0], $mensaje);
+        $mensaje = str_replace('[CUIDADOR_EMAIL]', $cuidador_email, $mensaje);
+
+
+        $mensaje = str_replace('[ORIGINAL]', $status_actual, $mensaje);
+        $mensaje = str_replace('[FINAL]', $new_status, $mensaje);
+        $mensaje = str_replace('[FECHA]', date("d/m/Y H:i a") , $mensaje);
+        
+        $mensaje = get_email_html($mensaje);    
+
+        wp_mail( "a.veloz@kmimos.la", "Actualización de Status", $mensaje);
+        wp_mail( "chaudaryy@gmail.com", "Actualización de Saldo", $mensaje);
     }
 
 	exit;
