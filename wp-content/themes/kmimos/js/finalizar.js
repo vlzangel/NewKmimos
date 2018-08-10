9b3ef5ae3c553d34f8c083398e5246d64b042923
Italo <italococchini@gmail.com>
2018-08-10 11:41:17 -0400

    ajustes cfdi

--- wp-content/themes/kmimos/js/finalizar.js
+++ wp-content/themes/kmimos/js/finalizar.js
@@ -3,4 +3,12 @@ jQuery(document).ready(function() {
 	jQuery(".navbar").addClass("bg-white-secondary");
 	jQuery('.navbar-brand img').attr('src', HOME+'/images/new/km-logos/km-logo-negro.png');
 
+
+	jQuery('[data-target="emitir_factura"]').on('click', function(){
+		jQuery.post(HOME+"/procesos/generales/activar_envio_cfdi.php", 
+		{  'user_id': jQuery(this).attr('data-id') },
+		function( data ){
+			jQuery('#emitir_factura').modal('show');
+		});
+	});
 });
