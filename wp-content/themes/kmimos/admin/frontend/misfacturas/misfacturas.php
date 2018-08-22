336d1eb406efe8f6ee247846534d74edeeb5dca0
Italo <italococchini@gmail.com>
2018-08-22 18:28:07 -0400

    filtro facturas cuidadores - listado de Meses

--- wp-content/themes/kmimos/admin/frontend/misfacturas/misfacturas.php
+++ wp-content/themes/kmimos/admin/frontend/misfacturas/misfacturas.php
@@ -126,7 +126,21 @@
 
 			<div class="contenedor-botones '.$ocultar.'">
 				<label>Filtrar por: </label>
-				<select data-action="filtro" name="filtro_mes" >'.$select_mes.'</select>
+				<select data-action="filtro" name="filtro_mes" >
+					<option value="0">Mes</option>
+					<option value="01">Enero</option>
+					<option value="02">Febrero</option>
+					<option value="03">Marzo</option>
+					<option value="04">Abril</option>
+					<option value="05">Mayo</option>
+					<option value="06">Junio</option>
+					<option value="07">Julio</option>
+					<option value="08">Agosto</option>
+					<option value="09">Septiembre</option>
+					<option value="10">Octubre</option>
+					<option value="11">Noviembre</option>
+					<option value="12">Diciembre</option>
+				</select>
 				<select data-action="filtro" name="filtro_anio" >'.$select_anio.'</select>
 
 				<button id="download-selected"><i class="fa fa-cloud-download"></i> Descargar en Zip</button>
