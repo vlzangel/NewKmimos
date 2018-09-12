1aba05f73dc21bc9be0288b41e2191e1498e3dc9
Italo <italococchini@gmail.com>
2018-09-11 22:50:55 -0400

    librerias para exportar - local

--- wp-content/plugins/kmimos/wlabel/index.php
+++ wp-content/plugins/kmimos/wlabel/index.php
@@ -19,13 +19,13 @@
 <!--
         <script src='<?php echo getTema(); ?>/admin/recursos/js/dataTables.buttons.min.js'></script>
  --> 
-<script src="https://cdn.datatables.net/buttons/1.5.2/js/dataTables.buttons.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.flash.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.html5.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.print.min.js"></script>
+<script src="<?php echo getTema(); ?>/admin/recursos/lib/dataTables.buttons.min.js"></script>
+<script src="<?php echo getTema(); ?>/admin/recursos/lib/buttons.flash.min.js"></script>
+<script src="<?php echo getTema(); ?>/admin/recursos/lib/jszip.min.js"></script>
+<script src="<?php echo getTema(); ?>/admin/recursos/lib/pdfmake.min.js"></script>
+<script src="<?php echo getTema(); ?>/admin/recursos/lib/vfs_fonts.js"></script>
+<script src="<?php echo getTema(); ?>/admin/recursos/lib/buttons.html5.min.js"></script>
+<script src="<?php echo getTema(); ?>/admin/recursos/lib/buttons.print.min.js"></script>
 
 
 
