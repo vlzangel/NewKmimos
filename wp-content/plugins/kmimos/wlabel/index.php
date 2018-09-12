e67418b22bc81618845d55aca0ea0e2c96393945
Italo <italococchini@gmail.com>
2018-09-11 23:18:02 -0400

    eTIQUETA DE EXPORTAR

--- wp-content/plugins/kmimos/wlabel/index.php
+++ wp-content/plugins/kmimos/wlabel/index.php
@@ -16,20 +16,15 @@
 
         <script src='<?php echo getTema(); ?>/admin/recursos/js/jquery.dataTables.min.js'></script>
         <script src='<?php echo getTema(); ?>/admin/recursos/js/dataTables.bootstrap4.min.js'></script>
-<!--
-        <script src='<?php echo getTema(); ?>/admin/recursos/js/dataTables.buttons.min.js'></script>
- --> 
-<script src="<?php echo getTema(); ?>/admin/recursos/lib/dataTables.buttons.min.js"></script>
-<script src="<?php echo getTema(); ?>/admin/recursos/lib/buttons.flash.min.js"></script>
-<script src="<?php echo getTema(); ?>/admin/recursos/lib/jszip.min.js"></script>
-<script src="<?php echo getTema(); ?>/admin/recursos/lib/pdfmake.min.js"></script>
-<script src="<?php echo getTema(); ?>/admin/recursos/lib/vfs_fonts.js"></script>
-<script src="<?php echo getTema(); ?>/admin/recursos/lib/buttons.html5.min.js"></script>
-<script src="<?php echo getTema(); ?>/admin/recursos/lib/buttons.print.min.js"></script>
-
-
-
-
+         
+        <script src="<?php echo getTema(); ?>/admin/recursos/lib/dataTables.buttons.min.js"></script>
+        <script src="<?php echo getTema(); ?>/admin/recursos/lib/buttons.flash.min.js"></script>
+        <script src="<?php echo getTema(); ?>/admin/recursos/lib/jszip.min.js"></script>
+        <script src="<?php echo getTema(); ?>/admin/recursos/lib/pdfmake.min.js"></script>
+        <script src="<?php echo getTema(); ?>/admin/recursos/lib/vfs_fonts.js"></script>
+        <script src="<?php echo getTema(); ?>/admin/recursos/lib/buttons.html5.min.js"></script>
+        <script src="<?php echo getTema(); ?>/admin/recursos/lib/buttons.print.min.js"></script>
+ 
         <link rel='stylesheet' type='text/css' href='<?php echo getTema(); ?>/admin/recursos/css/bootstrap.css'>
         <link rel='stylesheet' type='text/css' href='<?php echo getTema(); ?>/admin/recursos/css/dataTables.bootstrap4.min.css'>
         <link rel='stylesheet' type='text/css' href='<?php echo getTema(); ?>/admin/recursos/css/buttons.dataTables.min.css'>
