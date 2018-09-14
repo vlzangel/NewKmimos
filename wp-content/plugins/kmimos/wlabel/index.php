a604b409a297600f74fbd2cfa90bce0aca7f8397
Ángel Veloz <vlzangel91@gmail.com>
2018-09-14 12:47:37 -0430

    Act

--- wp-content/plugins/kmimos/wlabel/index.php
+++ wp-content/plugins/kmimos/wlabel/index.php
@@ -10,8 +10,11 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
-        <script src='<?php echo getTema(); ?>/admin/recursos/js/jquery.dataTables.min.js'></script>
-        <script src='<?php echo getTema(); ?>/admin/recursos/js/dataTables.bootstrap4.min.js'></script>
+        <!-- <script src='<?php echo getTema(); ?>/admin/recursos/js/jquery.dataTables.min.js'></script>
+        <script src='<?php echo getTema(); ?>/admin/recursos/js/dataTables.bootstrap4.min.js'></script> -->
+        
+        <script src='https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js'></script>
+        <script src='https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js'></script>
          
         <script src="<?php echo getTema(); ?>/admin/recursos/lib/dataTables.buttons.min.js"></script>
         <script src="<?php echo getTema(); ?>/admin/recursos/lib/buttons.flash.min.js"></script>
