45ddf0bd7ba33e3edec4e162e04933eb0fecb3ce
Italo <italococchini@gmail.com>
2018-09-21 16:31:08 -0400

    correo prueba

--- wp-content/themes/kmimos/template/mail/reservar/partes/datos_cuidador.php
+++ wp-content/themes/kmimos/template/mail/reservar/partes/datos_cuidador.php
@@ -11,7 +11,7 @@
 
         <table style='
             padding: 0px;
-            width: 520px;
+            width: 550px;
         '>
             <tr>
                 <td align="center" height="160" style="
