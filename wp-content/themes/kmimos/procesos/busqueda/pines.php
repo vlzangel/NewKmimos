e6280a29b37760e023967916ddf3318af24f8da8
Angel <vlzangel91@gmail.com>
2018-10-03 12:20:04 -0430

    Act

--- wp-content/themes/kmimos/procesos/busqueda/pines.php
+++ wp-content/themes/kmimos/procesos/busqueda/pines.php
@@ -2,9 +2,6 @@
 	session_start();
 
 	$pines = unserialize( $_SESSION['pines_array'] );
-	foreach ($pines as $key => $value) {
-		unset($pines[ $key ]["nom"] );
-	}
 
 	echo json_encode( $pines, JSON_UNESCAPED_UNICODE );
 ?>
