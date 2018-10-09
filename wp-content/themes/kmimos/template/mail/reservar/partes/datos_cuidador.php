1422a610a0cbe9165e5c847bbf1d85de00180a97
Italo Cocchini <italococchini@gmail.com>
2018-10-09 12:14:54 -0430

    ajustes email

--- wp-content/themes/kmimos/template/mail/reservar/partes/datos_cuidador.php
+++ wp-content/themes/kmimos/template/mail/reservar/partes/datos_cuidador.php
@@ -10,12 +10,12 @@
     ">
 
         <table style='
-            width: 485px;
             padding: 0px;
         '>
             <tr>
                 <td align="center" height="160"  style="
                     background-color: #e8e8e8;"
+                    text-align: center;
                 >
                    <img src="[AVATAR_CUIDADOR]" height="160" width="100%" > 
                 </td>
