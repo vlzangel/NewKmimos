faa097016cdd8fc0773059faffe828468b70fab6
Angel <vlzangel91@gmail.com>
2018-11-08 11:41:22 -0430

    Update wp-content/themes/kmimos/NEW/footer.php

--- wp-content/themes/kmimos/NEW/footer.php
+++ wp-content/themes/kmimos/NEW/footer.php
@@ -58,7 +58,7 @@
 						<h2>Contáctanos</h2>
 						<ul class="info_contacto">
 							<li> <a href="tel:018009564667" > <i class="fa fa-phone" aria-hidden="true"></i> <span>Teléfono:</span> 01 (800) 9 564667 </a> </li>
-							<li> <a href="tel:015585261162" > <i class="fa fa-phone" aria-hidden="true"></i> <span>Telef. Local:</span> 01 (55) 85261162 </a> </li>
+							<li> <a href="tel:+52015585261162" > <i class="fa fa-phone" aria-hidden="true"></i> <span>Telef. Local:</span> 01 (55) 85261162 </a> </li>
 							<li> <a href="whatsapp://send/?phone=+5213312614186" > <i class="fa fa-whatsapp" aria-hidden="true"></i> <span>WhatsApp:</span> +52 1 (33) 12614186 </a> </li>
 							<li> <a href="whatsapp://send/?phone=+5215568922182" > <i class="fa fa-whatsapp" aria-hidden="true"></i> <span>WhatsApp:</span> +52 1 (55) 68922182 </a> </li>
 							<li> <a href="whatsapp://send/?phone=+5215565602472" > <i class="fa fa-whatsapp" aria-hidden="true"></i> <span>WhatsApp:</span> +52 1 (55) 65602472 </a> </li>
