43ca97631a2dc6d8a567e1b530fd495bc2b7a5ef
Angel <vlzangel91@gmail.com>
2018-11-07 20:03:05 -0430

    Act

--- wp-content/themes/kmimos/page-testimonios.php
+++ wp-content/themes/kmimos/page-testimonios.php
@@ -13,6 +13,29 @@
     $user_id = get_current_user_id();
 
 	$HTML .= '
+	<style>
+		.pc_seccion_0 {
+		    width: 100%;
+		    height: 230px;
+		    background-size: cover;
+		    background-position: center;
+		    position: relative;
+		    z-index: -1;
+		}
+
+		.testimonios_container .testimonios_item {
+		    bottom: 25px;
+		}
+
+		.testimonios_container {
+		    padding: 50px;
+		    margin-bottom: 0px;
+		}
+	</style>
+	<div class="pc_seccion_0" style="background-image:url('.getTema().'/images/new/km-ficha/km-bg-ficha.jpg);">
+		<div class="overlay"></div>
+	</div>
+
 	<div class="testimonios_container">
 		<div class="testimonios_item">
 			<p>Por segunda vez dejé a mi perro con Gabriel y su familia, estoy muy agradecido y encantado con el cuidado que le ha dado a mi mascota. Durante toda la estadía me envió fotos de mi perrito feliz mientras yo viajaba.</p>
