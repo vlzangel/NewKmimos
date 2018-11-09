3d081f00bcdd636e85d79d3c0bf424dfd97a3985
Angel <vlzangel91@gmail.com>
2018-11-09 16:07:55 -0430

    Update wp-content/themes/kmimos/NEW/footer.php

--- wp-content/themes/kmimos/NEW/footer.php
+++ wp-content/themes/kmimos/NEW/footer.php
@@ -61,7 +61,7 @@
 							<li> <a href="tel:+52015585261162" > <i class="fa fa-phone" aria-hidden="true"></i> <span>Telef. Local:</span> 01 (55) 85261162 </a> </li>
 							<li> <a href="whatsapp://send/?phone=+5213312614186" > <i class="fa fa-whatsapp" aria-hidden="true"></i> <span>WhatsApp:</span> +52 1 (33) 12614186 </a> </li>
 							<li> <a href="whatsapp://send/?phone=+5215568922182" > <i class="fa fa-whatsapp" aria-hidden="true"></i> <span>WhatsApp:</span> +52 1 (55) 68922182 </a> </li>
-							<li> <a href="whatsapp://send/?phone=+5215565602472" > <i class="fa fa-whatsapp" aria-hidden="true"></i> <span>WhatsApp:</span> +52 1 (55) 65602472 </a> </li>
+							<li> <a href="whatsapp://send/?phone=+5215531374829" > <i class="fa fa-whatsapp" aria-hidden="true"></i> <span>WhatsApp:</span> +52 1 (55) 31374829 </a> </li>
 							<li> <a href="mailto:contactomex@kmimos.la" > <i class="fa fa-envelope-o" aria-hidden="true"></i> <span>Email:</span> contactomex@kmimos.la </a> </li>
 						</ul>
 					</div>
