8e6cddd552f83f2693385d1f9e312443b362814a
Angel <vlzangel91@gmail.com>
2018-11-14 17:01:42 -0430

    Copon 2

--- wp-content/themes/kmimos/procesos/reservar/cupon.php
+++ wp-content/themes/kmimos/procesos/reservar/cupon.php
@@ -113,12 +113,12 @@ ini_set('display_errors', '0');
 							}
 						}
 					}
-				} asort($noches);
+				} sort($noches);
 
 				if( $uso_cupon == false ){ if( count($noches) < 7 ){ if( $validar ){ error("El cupón [ {$cupon} ] sólo es válido si reservas 7 noches o más"); }else{ return false; } } }
 
 				if( $tipo_servicio == "hospedaje" ){
-					$descuento = $noches[0];
+					$descuento = $noches[0]; // CALCULO DESCUENTO
 				}else{ if( $uso_cupon != false ){
 					if( $validar ){ error("El cupón sólo es válido para servicios de hospedaje"); }else{ return false; }
 				} }
@@ -127,7 +127,7 @@ ini_set('display_errors', '0');
 				
 				$sub_descuento += $descuento;
 				$descuento += ( ($total-$sub_descuento) < 0 ) ? $descuento += ( $total-$sub_descuento ) : 0 ;
-				return array( $cupon, $descuento, $individual_use, $_noches, $uso_cupon );
+				return array( $cupon, $descuento, $individual_use, $_noches );
 			}
 
 			if( $cupon == "2pgpet" ){
@@ -135,13 +135,11 @@ ini_set('display_errors', '0');
 				if( !es_petco($db, $cliente) ){ if( $validar ){ error("El cupón solo es válido para usuarios de Petco"); }else{ return false; } }
 				if( !es_nuevo($db, $cliente) ){ if( $validar ){ error("El cupón solo es válido para usuarios nuevos"); }else{ return false; } }
 
-				$descuento = 0;
-				$usado = 0;
-				$cupon_post = $db->get_var("SELECT ID FROM wp_posts WHERE post_name = '{$cupon}'");
-				$cupon_meta = $db->get_results("SELECT * FROM wp_postmeta WHERE post_id = {$cupon_post} AND meta_key = '_used_by' AND meta_value = '{$cliente}'");
-				if( $cupon_meta != false ){
-					$usado = count($cupon_meta);
-				}
+				$descuento = 0; $_paseos = 2;
+				$uso_cupon = get_cupon($db, $cupon, $cliente);
+				if( $uso_cupon != false ){ $_paseos = $uso_cupon->disponible; }
+				if( $_paseos == 0 ){ if( $validar ){ error("Ya uso los 2 paseos gratis"); }else{ return false; } }
+
 				$paseos = [];
 				for ($i=0; $i < $duracion; $i++) { 
 					foreach ($mascotas as $key => $value) {
@@ -153,73 +151,22 @@ ini_set('display_errors', '0');
 							}
 						}
 					}
-				}
-				asort($paseos);
-				$duracion_con_mascotas = count($paseos);
-				if( $usado == 0 ){
-					if( $duracion_con_mascotas < 7 ){
-						if( $validar ){
-							echo json_encode(array(
-								"error" => "El cupón [ {$cupon} ] sólo es válido si reservas 7 noches o más"
-							));
-							exit;
-						}else{
-							return false;
-						}
-					}
-				}else{
-					if( $tipo_servicio == "paseos" ){
-						$data_cupon = json_decode( $db->get_var("SELECT meta_value FROM wp_postmeta WHERE post_id = {$cupon_post} AND meta_key = 'paseos_{$cliente}'") );
-						
-						if( $data_cupon->disponibles == 0 ){
-							if( $validar ){
-								echo json_encode(array(
-									"error" => "Ya uso los 2 paseos gratis"
-								));
-								exit;
-							}else{
-								return false;
-							}
-						}else{
-							$cont = 0;
-							foreach ($paseos as $key => $value) {
-								$descuento += $value;
-								$cont++;
-								if( $cont == $data_cupon->disponibles ){
-									break;
-								}
-							}
-						}
+				} sort($paseos);
 
-					}else{
-						if( $validar ){
-							echo json_encode(array(
-								"error" => "Solo puedes aplicar este cupón en servicios de paseo"
-							));
-							exit;
-						}else{
-							return false;
-						}
-					}
-				}
+				if( $uso_cupon == false ){ if( count($paseos) < 7 ){ if( $validar ){ error("El cupón [ {$cupon} ] sólo es válido si reservas 7 noches o más"); }else{ return false; } } }
 
+				if( $tipo_servicio == "paseos" ){
+					$descuento = $paseos[0]+$paseos[1]; // CALCULO DESCUENTO
+				}else{ if( $uso_cupon != false ){
+					if( $validar ){ error("El cupón sólo es válido para servicios de paseos"); }else{ return false; }
+				} }
+
+				if( $descuento > 0 ){ $_paseos = 0; }
+				
 				$sub_descuento += $descuento;
-				if( ($total-$sub_descuento) < 0 ){
-					$descuento += ( $total-$sub_descuento );
-				}
-				if( $metas["individual_use"] == "yes" ){
-					return array(
-						$cupon,
-						$descuento,
-						1
-					);
-				}else{
-					return array(
-						$cupon,
-						$descuento,
-						0
-					);
-				}
+				$descuento += ( ($total-$sub_descuento) < 0 ) ? $descuento += ( $total-$sub_descuento ) : 0 ;
+				return array( $cupon, $descuento, $individual_use, $_paseos, $paseos );
+
 			}
 
 			if( $cupon == "2ngpet" ){
