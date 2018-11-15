b34a5a39da58053bcc476b28133a827e16b99831
Angel <vlzangel91@gmail.com>
2018-11-15 14:44:11 -0430

    Update wp-content/themes/kmimos/single-petsitters.php

--- wp-content/themes/kmimos/single-petsitters.php
+++ wp-content/themes/kmimos/single-petsitters.php
@@ -23,6 +23,13 @@
 	$current_user = wp_get_current_user();
     $user_id = $current_user->ID;
 
+    if( is_user_logged_in() && $_SESSION["save_uso_banner"] ){
+	    set_uso_banner([
+    		"user_id" => $user_id
+    	]);
+    	unset($_SESSION["save_uso_banner"]);
+    }
+
 	if( $_cuidador->activo == 0 && $current_user->roles[0] != "administrator" ){
 		header("location: ".get_home_url());
 	}
