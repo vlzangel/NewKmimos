b9f7be61547065787ebedc6d2eed2cceac14a38a
Italo <italococchini@gmail.com>
2018-12-20 15:44:58 -0400

    act

--- wp-content/themes/kmimos/admin/backend/pagos/ajax/generar_solicitud.php
+++ wp-content/themes/kmimos/admin/backend/pagos/ajax/generar_solicitud.php
@@ -1,6 +1,8 @@
-<?php
-	session_start();
+<?php 
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 
+	session_start();
     date_default_timezone_set('America/Mexico_City');
 
     $raiz = dirname(dirname(dirname(dirname(dirname(dirname(dirname(__DIR__)))))));
