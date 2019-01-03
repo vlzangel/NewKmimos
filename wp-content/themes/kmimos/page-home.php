cf66cc231c80b7e593e3a0c508cb569d023d7a19
Italo <italococchini@gmail.com>
2019-01-03 12:12:51 -0400

    ajustes redireccion home

--- wp-content/themes/kmimos/page-home.php
+++ wp-content/themes/kmimos/page-home.php
@@ -316,7 +316,7 @@
 
 		if( time() > strtotime("2018-11-16 00:00:00") ){
 			$HTML .= '
-				<a href="https://www.nomadas.life/?publicmap=kmimos" style="display: block;">
+				<a href="'.get_home_url().'/redireccion/?url=https://www.nomadas.life/?publicmap=kmimos" style="display: block;">
 					<img src="'.get_recurso("img").'BANNERS/banner_rotativo/pc/4.jpg" width="100%" class="solo_pc" />
 					<img src="'.get_recurso("img").'BANNERS/banner_rotativo/movil/4.jpg" width="100%" class="solo_movil" />
 				</a>
