a4a7bdd759fc6fcaf12071e9a4e97cbf2c1f6f3d
Angel Veloz <vlzangel91@gmail.com>
2019-01-14 16:39:17 -0400

    Create c.php

---
+++ c.php
@@ -0,0 +1,9 @@
+<?php
+	include 'wp-load.php';
+	
+	if( !isset($_SESSION) ){ session_start(); }
+
+	$_temp = pre_carga_data_cuidadores();
+	$_SESSION["DATA_CUIDADORES"] = $_temp[0];
+	$_SESSION["CUIDADORES_USER_ID"] = $_temp[1];
+?>
