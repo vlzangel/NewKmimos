91babe646433736d6473de53be6038064914182d
Angel Veloz <vlzangel91@gmail.com>
2019-02-22 16:16:05 -0400

    Update index.php

--- petco/index.php
+++ petco/index.php
@@ -34,7 +34,12 @@
 	// echo "Landing: {$ult_landing}<br>";
 
 	$param = ( !empty($_SERVER['QUERY_STRING']) && isset($_GET['utm_campaign']) )? '&'.$_SERVER['QUERY_STRING'] : '&utm_source=web&utm_medium=banner&utm_campaign=petco_kmimos&utm_term=white_label_petco' ;	
-	$url = get_home_url().'/?landing='.$ult_landing.'&wlabel=petco'.$param;	
+	
+	if( $ult_landing == 'a' ){
+		$url = get_home_url().'/?wlabel=petco'.$param;	
+	}else{
+		$url = get_home_url().'/?landing='.$ult_landing.'&wlabel=petco'.$param;	
+	}	
 
 	header('Location:'.$url );
 
