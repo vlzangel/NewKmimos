bc50a588ac5b4919b397baab6c7e919380c8c06b
Angel Veloz <vlzangel91@gmail.com>
2019-02-26 13:15:30 -0400

    Act

--- wp-content/themes/kmimos/page-home_2.php
+++ wp-content/themes/kmimos/page-home_2.php
@@ -37,7 +37,7 @@
     ];
     foreach ($info_banner as $key => $url) {
     	$items .= '<div class="banner_rotativo_item solo_pc_banner" style="background-image: url('.get_recurso('img').'HOME_2/Muestra'.$url.');"></div>';
-    	$items .= '<div class="banner_rotativo_item solo_movil_banner" style="background-image: url('.get_recurso('img').'HOME_2/RESPONSIVE/Muestra'.$url.');"></div>';
+    	$items .= '<div class="banner_rotativo_item solo_movil_banner"> <img src="'.get_recurso('img').'HOME_2/RESPONSIVE/Muestra'.$url.'" /> </div>';
     }
 
     $items_count = count($info_banner);
