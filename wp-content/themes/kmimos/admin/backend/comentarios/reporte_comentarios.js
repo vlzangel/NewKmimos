3d82eaed82f6fadd01df5c1ea11fba6461e4302d
Italo <italococchini@gmail.com>
2019-03-12 15:24:36 -0400

    Reportes con comentarios

---
+++ wp-content/themes/kmimos/admin/backend/comentarios/reporte_comentarios.js
@@ -0,0 +1,66 @@
+var table = ""; 
+
+jQuery(document).ready(function() {
+	loadTabla();
+});
+
+function loadTabla( tipo, hiddenColumns ){
+ 	 
+	table = jQuery('#example').DataTable();
+	table.destroy();
+
+    table = jQuery('#example').DataTable({
+    	"language": {
+			"emptyTable":			"No hay datos disponibles en la tabla.",
+			"info":		   			"Del _START_ al _END_ de _TOTAL_ ",
+			"infoEmpty":			"Mostrando 0 registros de un total de 0.",
+			"infoFiltered":			"(filtrados de un total de _MAX_ registros)",
+			"infoPostFix":			" (actualizados)",
+			"lengthMenu":			"Mostrar _MENU_ registros",
+			"loadingRecords":		"Cargando...",
+			"processing":			"Procesando...",
+			"search":				"Buscar:",
+			"searchPlaceholder":	"Dato para buscar",
+			"zeroRecords":			"No se han encontrado coincidencias.",
+			"paginate": {
+				"first":			"Primera",
+				"last":				"Última",
+				"next":				"Siguiente",
+				"previous":			"Anterior"
+			},
+			"aria": {
+				"sortAscending":	"Ordenación ascendente",
+				"sortDescending":	"Ordenación descendente"
+			}
+		},
+		"dom": '<B><f><t>ip',
+		"buttons": [
+			{
+			  extend: "csv",
+			  className: "btn-sm"
+			},
+			{
+			  extend: "excelHtml5",
+			  className: "btn-sm"
+			},
+        ],
+		
+        "scrollX": true,
+        "ajax": {
+            "url": TEMA+'/admin/backend/comentarios/ajax/comentarios.php',
+            "data": {},
+            "type": "POST"
+        }
+	});
+
+}
+
+
+
+
+
+
+
+
+
+ 
