b15022a92f2e419bf283824a23222cce306601d0
Angel Veloz <vlzangel91@gmail.com>
2019-03-13 13:39:51 -0400

    Update page-paseos.php

--- wp-content/themes/kmimos/page-paseos.php
+++ wp-content/themes/kmimos/page-paseos.php
@@ -3,6 +3,8 @@
         Template Name: Paseos
     */
 
+    wp_enqueue_style('home_club_responsive', getTema()."/css/responsive/club_patitas_home.css", array(), '1.0.0');
+    
     wp_enqueue_style('home_kmimos', get_recurso("css")."paseos.css", array(), '1.0.0');
     wp_enqueue_style('home_responsive', get_recurso("css")."responsive/paseos.css", array(), '1.0.0');
 	wp_enqueue_style( 'bootstrap.min', getTema()."/css/bootstrap.min.css", array(), "1.0.0" );
@@ -497,6 +499,26 @@
 
 		$HTML .= '
 		<!-- SECCIÓN 4 - CLUB PATITAS FELICES -->
+		<div class="km-club-patitas" style="background-image: url('.getTema().'/images/club-patitas/Kmimos-Club-de-las-patitas-felices-3.png );"> 
+			<div class="row"> 
+				<header class="col-sm-12 col-xs-8 col-md-5 pull-right text-center"> 
+					<img src="'.getTema().'/images/club-patitas/Kmimos-Club-de-las-patitas-felices-5.png"> 
+					<h2> Club de las patitas felices </h2> 
+					<p> Únete al club que te recompensa por cada amigo tuyo que reserve con un cuidador Kmimos </p> 
+				</header> 
+			</div> 
+			<div class="row"> 
+				<div class="col-sm-6 col-xs-12 col-md-5 pull-right text-center"> 
+					<a class="btn btn-club-patitas" href="'.get_home_url().'/club-patitas-felices/?utm_source=homepage&amp;utm_medium=banner&amp;utm_campaign=club-patitas-felices" target="_blanck">
+						Ingresa aquí
+					</a> 
+				</div>
+			</div> 
+		</div>
+		<!-- FIN SECCIÓN 4 - CLUB PATITAS FELICES -->';
+/*
+		$HTML .= '
+		<!-- SECCIÓN 4 - CLUB PATITAS FELICES -->
 		<div class="club_patitas_container_superior">
 			<div class="club_patitas_container">
 				<div class="club_patitas_tabla">
@@ -525,7 +547,7 @@
 			</div>
 		</div>
 		<!-- FIN SECCIÓN 4 - CLUB PATITAS FELICES -->';
-
+*/
 		/*
 		$HTML .= '
 
@@ -612,6 +634,7 @@
     echo comprimir($HTML);
     
     wp_enqueue_script('buscar_home', get_recurso("js")."paseos.js", array(), '1.0.0');
+    wp_enqueue_script('club_patitas', get_recurso("js")."club_patitas.js", array(), '1.0.0');
 
     get_footer(); 
 ?>
