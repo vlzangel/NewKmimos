ad5156a64ac207de0dec20ae3e53d4f16a6af4cb
Angel Veloz <vlzangel91@gmail.com>
2019-03-18 18:56:15 -0400

    Update p.php

--- p.php
+++ p.php
@@ -3,23 +3,47 @@
 	
 	if( !isset($_SESSION) ){ session_start(); }
 
+	global $wpdb;
+
 	// update_ubicacion();
 	// update_titulo();
 	// update_cuidador_url();
-
 	// update_servicios();
-
 	// pre_carga_data_cuidadores();
-
 	// echo serialize([ "subscriber" => true ]);
-
 	// update_titulo();
-	
 	// update_precios_paseos();
 
-	$_cuidador = $_SESSION["DATA_CUIDADORES"][ $_SESSION["CUIDADORES_USER_ID"][ $post->post_author ] ];
-
+	/*
 	echo '<pre>';
 		print_r($_SESSION["DATA_CUIDADORES"]);
 	echo '</pre>';
+	*/
+
+	$cuidadores = $wpdb->get_results("SELECT * FROM cuidadores_2 WHERE activo = 1");
+	$cambios_total = 0;
+	foreach ($cuidadores as $indice => $cuidador) {
+		if( $cuidador->paseos_desde > 0 && $cuidador->paseos_desde < 40 ){
+			$cuidador->paseos_desde = 40;
+			$wpdb->query("UPDATE cuidadores_2 SET paseos_desde = '40' WHERE id = ".$cuidador->id);
+		}
+		$adicionales = unserialize($cuidador->adicionales);
+		$cambio = false;
+		foreach ($adicionales as $key => $value) {
+			if( count($value) > 3 ){
+				foreach ($value as $key_2 => $value_2 ) {
+					if( $value_2 > 0 && $value_2 < 40 ){
+						$adicionales[ $key ][ $key_2 ] = 40;
+						$cambio = true;
+					}
+				}
+			}
+		}
+		if( $cambio ){
+			$cambios_total++;
+			$cuidadores[ $indice ]->adicionales = serialize( $adicionales );
+			$wpdb->query("UPDATE cuidadores_2 SET adicionales = '".serialize( $adicionales )."' WHERE id = ".$cuidador->id);
+		}
+	}
+	echo $cambios_total;
 ?>
