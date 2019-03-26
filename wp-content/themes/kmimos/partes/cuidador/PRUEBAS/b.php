7f981d3d55b4c94027966cf40d32e7c127fbf4a5
Angel Veloz <vlzangel91@gmail.com>
2019-03-26 18:33:48 -0400

    Act

--- wp-content/themes/kmimos/partes/cuidador/PRUEBAS/b.php
+++ wp-content/themes/kmimos/partes/cuidador/PRUEBAS/b.php
@@ -3,11 +3,11 @@
 		<div class="conocer_b">
 			<div class="titulo_b">Cómo conocer al Cuidador <strong><span id="modal-name-cuidador"></span></strong> antes de dejar a su cuidado a tu amigo peludo.</div>
 			<ul>
-				<li> <strong>1</strong> Realiza tu reserva con el Cuidador</li>
-				<li> <strong>2</strong> En tu perfil Kmims, ingresa a la sección Reservas</li>
-				<li> <strong>3</strong> Ve a <span>reservas confirmadas</span> y selecciona la que corresponda</li>
-				<li> <strong>4</strong> Selecciona la opción <strog>conocer cuidador</strong></li>
-				<li> <strong>5</strong> Llena la solicitud y <span>listo</span></li>
+				<li> <strong>1</strong> <div> Realiza tu reserva con el Cuidador </div> </li>
+				<li> <strong>2</strong> <div> En tu perfil Kmims, ingresa a la sección Reservas </div> </li>
+				<li> <strong>3</strong> <div> Ve a <span>reservas confirmadas</span> y selecciona la que corresponda </div> </li>
+				<li> <strong>4</strong> <div> Selecciona la opción <strog>conocer cuidador</strong> </div> </li>
+				<li> <strong>5</strong> <div> Llena la solicitud y <span>listo</span> </div> </li>
 			</ul>
 			<div class="mensaje_b">
 				Y no te preocupes, si después de haberlo(a) conocido no te convence, puedes elegir a otro cuidador o cancelar tu reservación obteniendo un reembolso completo de tu pago.
