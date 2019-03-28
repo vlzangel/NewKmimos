34b8dc740c05494af29ef72844c4701b74a9fedf
Italo <italococchini@gmail.com>
2019-03-27 21:33:51 -0400

    panel - act style load

--- wp-content/themes/kmimos/widgets/views/resumen-registro.php
+++ wp-content/themes/kmimos/widgets/views/resumen-registro.php
@@ -1,11 +1,4 @@
-<?php 
-	$theme = 'light';
-	// $theme = 'dark'; 
-?>
-
-<link rel="stylesheet" type="text/css" href="<?php echo get_recurso('css'); ?>/widgets/panel.css">
-<link rel="stylesheet" type="text/css" href="<?php echo get_recurso('css'); ?>/widgets/<?php echo $theme; ?>.css">
-
+ 
 <section class="container ">
 	<aside>
 		<button type="button" data-target="registro-change" data-id="byDay" class="btn ">Diario</button>
