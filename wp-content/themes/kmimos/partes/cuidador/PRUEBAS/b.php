642ce524cfde06f4d039131485c7ad5fef790158
Angel Veloz <vlzangel91@gmail.com>
2019-03-28 11:44:27 -0400

    Act conocer

--- wp-content/themes/kmimos/partes/cuidador/PRUEBAS/b.php
+++ wp-content/themes/kmimos/partes/cuidador/PRUEBAS/b.php
@@ -4,7 +4,7 @@
 			<div class="titulo_b">Cómo conocer al Cuidador <strong><span id="modal-name-cuidador"></span></strong> antes de dejar a su cuidado a tu amigo peludo.</div>
 			<ul>
 				<li> <strong>1</strong> <div> Realiza tu reserva con el Cuidador </div> </li>
-				<li> <strong>2</strong> <div> En tu perfil Kmims, ingresa a la sección Reservas </div> </li>
+				<li> <strong>2</strong> <div> En tu perfil Kmimos, ingresa a la sección Reservas </div> </li>
 				<li> <strong>3</strong> <div> Ve a <span>reservas confirmadas</span> y selecciona la que corresponda </div> </li>
 				<li> <strong>4</strong> <div> Selecciona la opción <strog>conocer cuidador</strong> </div> </li>
 				<li> <strong>5</strong> <div> Llena la solicitud y <span>listo</span> </div> </li>
@@ -13,8 +13,8 @@
 				Y no te preocupes, si después de haberlo(a) conocido no te convence, puedes elegir a otro cuidador o cancelar tu reservación obteniendo un reembolso completo de tu pago.
 			</div>
 			<div class="botones_b">
-				<a href="" class="boton boton_izq">Reservar y conocer</a>
-				<span class="boton boton_der">He hecho la reserva</span>
+				<a href="#" class="boton boton_izq">Reservar y conocer</a>
+				<a href="'.get_home_url().'/perfil-usuario/historial/" class="boton boton_der">He hecho la reserva</a>
 			</div>
 		</div> 
 	';
