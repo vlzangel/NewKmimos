445eee8ac601bac4d2b1cda90b9f6e5a0003886d
Angel Veloz <vlzangel91@gmail.com>
2019-04-26 16:49:27 -0400

    Act

--- wp-content/themes/kmimos/admin/frontend/reservas/cancelar.php
+++ wp-content/themes/kmimos/admin/frontend/reservas/cancelar.php
@@ -10,12 +10,12 @@
 
     include($PATH_TEMPLATE."/procesos/reservar/emails/index.php");
 
-    if( $_GET["CONFIRMACION"] == "YES" ){
+    // if( $_GET["CONFIRMACION"] == "YES" ){
     	$CONTENIDO .= "<a class='km-btn-primary volver_msg' href='".get_home_url()."/perfil-usuario/reservas/'>Volver</a>";
-    }else{
+    /* }else{
     	$CONTENIDO .= "
     		<a style='display: inline-block; margin-left: 15px !important;' class='km-btn-primary volver_msg' href='".get_home_url()."/perfil-usuario/reservas/cancelar/".$id_orden."?u=cui&CONFIRMACION=YES'>SI</a>
     		<a class='km-btn-primary volver_msg' href='".get_home_url()."/perfil-usuario/reservas/'>NO</a>
     	";
-    }
+    } */
 ?>
