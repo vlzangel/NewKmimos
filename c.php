443ab55756fe39bbef50d4fb0fc77ae82aeb9fc9
Angel Veloz <vlzangel91@gmail.com>
2019-04-29 12:57:11 -0400

    Update c.php

--- c.php
+++ c.php
@@ -21,7 +21,7 @@
 
 	date_default_timezone_set('America/Mexico_City');
 
-	echo "Fecha: ".date("d/m/Y H:i:s", 1556554713);
+	echo "Fecha: ".date("d/m/Y H:i:s", 1556554713)."<br>";
 	echo "Fecha Ahora: ".date("d/m/Y H:i:s", time() );
 
 	/*
