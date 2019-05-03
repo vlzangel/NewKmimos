5d25c39e7148014f5c3285ea8fe198da4cf804a8
Angel Veloz <vlzangel91@gmail.com>
2019-05-03 19:39:05 -0400

    Act

--- wp-content/themes/kmimos/procesos/reservar/emails/pendientes/mercadopago.php
+++ wp-content/themes/kmimos/procesos/reservar/emails/pendientes/mercadopago.php
@@ -1,4 +1,4 @@
 <?php
 	$admins = kmimos_get_mail_admins();
-	wp_mail( 'a.veloz@kmimos.la', "Pendiente de pago mercadopago", "Reserva #{$RESERVA_ID} pendiente de pago mercadopago", $admins);
+	wp_mail( 'soporte.kmimos@gmail.com', "Pendiente de pago mercadopago", "Reserva #{$RESERVA_ID} pendiente de pago mercadopago", $admins);
 ?>
--- wp-content/themes/kmimos/procesos/reservar/emails/pendientes/paypal.php
+++ wp-content/themes/kmimos/procesos/reservar/emails/pendientes/paypal.php
@@ -1,4 +1,4 @@
 <?php
 	$admins = kmimos_get_mail_admins();
-	wp_mail( 'a.veloz@kmimos.la', "Pendiente de pago paypal", "Reserva #{$RESERVA_ID} pendiente de pago paypal", $admins);
+	wp_mail( 'soporte.kmimos@gmail.com', "Pendiente de pago paypal", "Reserva #{$RESERVA_ID} pendiente de pago paypal", $admins);
 ?>
