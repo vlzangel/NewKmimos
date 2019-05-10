f0d329b3a44d0a620ef8f530bd277a3f55e5eb5b
Angel Veloz <vlzangel91@gmail.com>
2019-05-10 18:08:12 -0400

    Update producto.js

--- wp-content/themes/kmimos/js/producto.js
+++ wp-content/themes/kmimos/js/producto.js
@@ -673,9 +673,11 @@ function pagarReserva(id_invalido = false){
 				}
 			}else{
 				CARRITO["pagar"]["id_fallida"] = 0;
-				if(data.url_pago == "" ){
+				if(data.url_pago == undefined && data.url_pago == "" ){
+					console.log("1");
 					// location.href = RAIZ+"/finalizar/"+data.order_id;
 				}else{
+					console.log("2");
 					// location.href = data.url_pago;
 				}
 			}
