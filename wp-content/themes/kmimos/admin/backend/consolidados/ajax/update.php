f081e3393860bba29925adf4fa511c5c2f9ace2c
Angel Veloz <vlzangel91@gmail.com>
2019-05-22 17:12:47 -0400

    Act

--- wp-content/themes/kmimos/admin/backend/consolidados/ajax/update.php
+++ wp-content/themes/kmimos/admin/backend/consolidados/ajax/update.php
@@ -1,31 +1,21 @@
 <?php
-
-    date_default_timezone_set('America/Mexico_City');
-
     $raiz = dirname(dirname(dirname(dirname(dirname(dirname(dirname(__DIR__)))))));
     include_once($raiz."/wp-load.php");
-
+    date_default_timezone_set('America/Mexico_City');
     global $wpdb;
-
     extract($_POST);
-
-    $info = $wpdb->get_row("SELECT * reporte_reserva WHERE id = {$id}");
-
-    // $valor = ( $campo == 'comentarios' ) ? $info->comentarios.$valor."\n\n" : $valor;
-
+    $info = $wpdb->get_row("SELECT * reporte_reserva_new WHERE id = {$id}");
     $SQL = "
         UPDATE 
-            reporte_reserva 
+            reporte_reserva_new 
         SET 
             {$campo} = '{$valor}'
         WHERE 
             id = {$id}
     ";
-
     $wpdb->query( $SQL );
-
     echo json_encode([
-        "SQL" => $SQL
+        "SQL" => $SQL,
+        "status" => "ok"
     ]);
-
 ?>
