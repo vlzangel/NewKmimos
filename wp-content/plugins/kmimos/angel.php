bab3d636cf144c5f80bb0e78683708fdc657f64d
Angel <vlzangel91@gmail.com>
2019-06-11 17:59:34 -0400

    Act

--- wp-content/plugins/kmimos/angel.php
+++ wp-content/plugins/kmimos/angel.php
@@ -144,9 +144,9 @@
     if(!function_exists('kmimos_get_mail_admins')){       
         function kmimos_get_mail_admins(){ 
             return array(
-                'BCC: vlzangel91@gmail.com',
-                // 'BCC: a.veloz@kmimos.la',
-                // 'BCC: y.chaudary@kmimos.la',
+                // 'BCC: vlzangel91@gmail.com',
+                'BCC: a.veloz@kmimos.la',
+                'BCC: y.chaudary@kmimos.la',
             );
         }
     }
