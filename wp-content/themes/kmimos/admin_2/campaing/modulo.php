bc07832261b0da144d35d3408c089b10097d334d
Ángel Veloz <vlzangel91@gmail.com>
2019-07-05 12:51:24 -0400

    Act

--- wp-content/themes/kmimos/admin_2/campaing/modulo.php
+++ wp-content/themes/kmimos/admin_2/campaing/modulo.php
@@ -123,7 +123,50 @@
 	                imageUploadURL: ADMIN_AJAX+"?action=vlz_campaing_uploads_list",
 	                imageUploadParams: {
 	                    id: "contenido"
-	                }
+	                },
+
+					imageUploadMethod: "POST",
+					imageMaxSize: 5 * 1024 * 1024,
+					imageAllowedTypes: ["jpeg", "jpg", "png"],
+
+					events: {
+						"image.beforeUpload": function (images) {
+							// Return false if you want to stop the image upload.
+						},
+						"image.uploaded": function (response) {
+							// Image was uploaded to the server.
+						},
+						"image.inserted": function ($img, response) {
+							// Image was inserted in the editor.
+						},
+						"image.replaced": function ($img, response) {
+							// Image was replaced in the editor.
+						},
+						"image.error": function (error, response) {
+							// Bad link.
+						if (error.code == 1) { ... }
+
+							// No link in upload response.
+						else if (error.code == 2) { ... }
+
+							// Error during image upload.
+						else if (error.code == 3) { ... }
+
+							// Parsing response failed.
+						else if (error.code == 4) { ... }
+
+							// Image too text-large.
+						else if (error.code == 5) { ... }
+
+							// Invalid image type.
+						else if (error.code == 6) { ... }
+
+							// Image can be uploaded only to same domain in IE 8 and IE 9.
+						else if (error.code == 7) { ... }
+
+							// Response contains the original server response to the request if available.
+						}
+					}
 	            });
 			</script>
 		';
