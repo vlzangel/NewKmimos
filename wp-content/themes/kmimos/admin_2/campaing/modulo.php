02c87e289697bbe45556541c6e9f8ae68d05c52f
Ángel Veloz <vlzangel91@gmail.com>
2019-07-05 12:54:00 -0400

    Act

--- wp-content/themes/kmimos/admin_2/campaing/modulo.php
+++ wp-content/themes/kmimos/admin_2/campaing/modulo.php
@@ -131,40 +131,21 @@
 
 					events: {
 						"image.beforeUpload": function (images) {
-							// Return false if you want to stop the image upload.
 						},
 						"image.uploaded": function (response) {
-							// Image was uploaded to the server.
 						},
 						"image.inserted": function ($img, response) {
-							// Image was inserted in the editor.
 						},
 						"image.replaced": function ($img, response) {
-							// Image was replaced in the editor.
 						},
 						"image.error": function (error, response) {
-							// Bad link.
-						if (error.code == 1) { ... }
-
-							// No link in upload response.
-						else if (error.code == 2) { ... }
-
-							// Error during image upload.
-						else if (error.code == 3) { ... }
-
-							// Parsing response failed.
-						else if (error.code == 4) { ... }
-
-							// Image too text-large.
-						else if (error.code == 5) { ... }
-
-							// Invalid image type.
-						else if (error.code == 6) { ... }
-
-							// Image can be uploaded only to same domain in IE 8 and IE 9.
-						else if (error.code == 7) { ... }
-
-							// Response contains the original server response to the request if available.
+						if (error.code == 1) {  }
+						else if (error.code == 2) {  }
+						else if (error.code == 3) {  }
+						else if (error.code == 4) {  }
+						else if (error.code == 5) {  }
+						else if (error.code == 6) {  }
+						else if (error.code == 7) {  }
 						}
 					}
 	            });
