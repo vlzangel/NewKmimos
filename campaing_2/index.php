37555e22af4d1c7ae1edb2c904f5671b22d81a7a
Angel Veloz <vlzangel91@gmail.com>
2019-07-24 22:47:26 -0400

    Update index.php

--- campaing_2/index.php
+++ campaing_2/index.php
@@ -13,7 +13,7 @@
 	if( is_array($d['vistos']) ){
 		if( !in_array($email, $d['vistos']) ){ $d['vistos'][] = [ "fecha" => time(), "email" => $email ]; }
 	}else{
-		$d['vistos'] = [ [ "fecha" => time(), "email" => $email ] ]
+		$d['vistos'] = [ [ "fecha" => time(), "email" => $email ] ];
 	}
 	$data = json_encode($d, JSON_UNESCAPED_UNICODE);
 	$data = str_replace('<p data-f-id=\"pbf\" style=\"text-align: center; font-size: 14px; margin-top: 30px; opacity: 0.65; font-family: sans-serif;\">Powered by <a href=\"https:\/\/www.froala.com\/wysiwyg-editor?pb=1\" title=\"\"><\/a><\/p>', '', $data);
