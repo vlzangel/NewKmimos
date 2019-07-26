8ffeafdf78cad12aff026cf47c68ee2cf5b59277
Angel Veloz <vlzangel91@gmail.com>
2019-07-26 15:43:03 -0400

    Update index.php

--- campaing_2/index.php
+++ campaing_2/index.php
@@ -7,6 +7,12 @@
 	$campaing = $db->get_row("SELECT * FROM vlz_campaing WHERE id = {$id}");
 	$d = (array) json_decode( utf8_encode($campaing->data) );
 
+	if( is_array($d['vistos']) ){
+		if( !in_array($email, $d['vistos']) ){ $d['vistos'][] = [ "fecha" => time(), "email" => $email ]; }
+	}else{
+		$d['vistos'] = [ [ "fecha" => time(), "email" => $email ] ];
+	}
+
 	
 	echo "<pre>";
 		print_r( $campaing );
@@ -16,12 +22,6 @@
 
 	exit();
 
-
-	if( is_array($d['vistos']) ){
-		if( !in_array($email, $d['vistos']) ){ $d['vistos'][] = [ "fecha" => time(), "email" => $email ]; }
-	}else{
-		$d['vistos'] = [ [ "fecha" => time(), "email" => $email ] ];
-	}
 	$data = json_encode($d, JSON_UNESCAPED_UNICODE);
 	$data = str_replace('<p data-f-id=\"pbf\" style=\"text-align: center; font-size: 14px; margin-top: 30px; opacity: 0.65; font-family: sans-serif;\">Powered by <a href=\"https:\/\/www.froala.com\/wysiwyg-editor?pb=1\" title=\"\"><\/a><\/p>', '', $data);
 	$sql = "UPDATE vlz_campaing SET data = '{$data}' WHERE id = ".$id;
