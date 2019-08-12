8bf06de1f7975fd076c376909f5ff9fbc209c9dd
Angel Veloz <vlzangel91@gmail.com>
2019-08-12 15:52:32 -0400

    Act

--- openpay/c_4.php
+++ openpay/c_4.php
@@ -14,8 +14,8 @@
 	Openpay::setProductionMode( true );
 
 	$findDataRequest = array(
-    	'creation[gte]' => '2017-01-01',
-    	'creation[lte]' => '2017-06-01',
+    	'creation[gte]' => '2018-06-01',
+    	'creation[lte]' => '2018-12-01',
     	'limit' => 5000
     );
 	
@@ -24,7 +24,6 @@
 	
 	$ORDER = get_user_meta(367, 'ORDER', true);
 	if( $ORDER+0 == 0 ){
-		// $_SESSION["ORDER"] = 2;
 		update_user_meta(367, 'ORDER', 9);
 	}
 
@@ -63,32 +62,4 @@
 
 		echo "Total: ".$total;
 	echo "</pre>";
-
-	/*
-		$order_id = str_pad('1', 6, "0", STR_PAD_LEFT);
-
-		$feeDataRequest = array(
-		    'customer_id' => 'ajsz254ilsdoe37wh5jc',
-		    'amount' => '1125',
-		    'description' => 'Cobro de Comisión',
-		    'order_id' => 'ORDEN-'.$order_id
-		);
-		$fee = $openpay->fees->create($feeDataRequest);
-
-		  3074,10 + 6646,25		01/19
-		  9720,35 + 216206,51	12/18
-		225926,86 + 157659,39	11/18
-		383586.25 + 70821,51	10/18
-		454407,76 + 33995		09/18
-		488402,76 + 33176,25	08/18
-		521579,01 + 229733,78	07/18
-		751312,79 + 73866,28	06/18
-		825179,07 + 7930,5	    05/18
-		833109,57
-
-
-		UPDATE `wp_usermeta` SET `meta_key` = '_openpay_customer_id_bk' WHERE `wp_usermeta`.`meta_key` = '_openpay_customer_id'
-	*/
-
-
 ?>
