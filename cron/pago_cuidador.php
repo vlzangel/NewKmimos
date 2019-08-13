0f0ce1f51d543a26d866a622b00a7186db87081b
Angel Veloz <vlzangel91@gmail.com>
2019-08-13 16:02:32 -0400

    Update pago_cuidador.php

--- cron/pago_cuidador.php
+++ cron/pago_cuidador.php
@@ -7,11 +7,15 @@ $tema = $raiz . '/wp-content/themes/kmimos';
 include_once($tema."/procesos/funciones/db.php");
 include_once($tema."/procesos/funciones/generales.php");
 include_once($tema."/procesos/funciones/config.php");
-include_once($tema.'/lib/openpay/Openpay.php');
+include_once($tema.'/lib/openpay2/Openpay.php');
+
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
 
 // credenciales testing -----------------------------------------
 // $openpay = Openpay::getInstance('mbkjg8ctidvv84gb8gan', 'sk_883157978fc44604996f264016e6fcb7');
 // --------------------------------------------------------------
+
 
 $openpay = Openpay::getInstance($MERCHANT_ID, $OPENPAY_KEY_SECRET);
 Openpay::setProductionMode( ($OPENPAY_PRUEBAS == 0) );
