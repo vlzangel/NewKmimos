f7b93ac3e18fbbe6827d650deb7d292d265da17b
Angel Veloz <vlzangel91@gmail.com>
2019-08-22 18:40:01 -0400

    Update redi.php

--- campaing_2/redi.php
+++ campaing_2/redi.php
@@ -3,11 +3,38 @@
 	include dirname(__DIR__).'/campaing/db.php';
 	$info = (array) json_decode(base64_decode( $_GET['info']));
 	extract($info);
-	
+
+	/*
 	echo "<pre>";
 		print_r( $info );
 	echo "</pre>";
-	
+	*/
 
-	// header("location: ".$info['url']);
+	$link = $db->get_row("SELECT * FROM vlz_seguimiento_links WHERE campaing = '{$info['id']}' AND email = '{$info['email']}' AND link = '{$info['url']}' ");
+	if( $link !== false ){
+		$_metas = json_decode($link->metadata);
+		$_metas->clicks += 1;
+		$_metas = json_encode($_metas);
+		/*
+		echo "<pre>";
+			print_r($_metas);
+		echo "</pre>";
+		*/
+		$db->query("UPDATE vlz_seguimiento_links SET actualizacion = NOW(), metadata = '{$_metas}' WHERE id = '{$link->id}';");
+	}else{
+		$metas = json_encode([
+			"clicks" => 1
+		]);
+		$db->query("INSERT INTO vlz_seguimiento_links VALUES (
+			NULL,
+			'{$info['id']}',
+			'{$info['email']}',
+			'{$info['url']}',
+			'{$metas}',
+			NOW(),
+			NOW()
+		);");
+	}
+	
+	header("location: ".$info['url']);
 ?>
