68f8fb56ce7a5d39b53afb760dee21dc2a5be92d
Angel Veloz <vlzangel91@gmail.com>
2019-09-20 14:04:58 -0400

    Update campaings_2.php

--- cron/campaing/campaings_2.php
+++ cron/campaing/campaings_2.php
@@ -22,6 +22,18 @@
 		switch ( $data->hacer_despues+0 ) {
 			case 1:
 
+				$padre_id_solo = $data->campaing_anterior;
+				$padre_id = "padre_".$padre_id_solo;
+				$otro_flujo = $wpdb->get_row("SELECT * FROM vlz_campaing WHERE data LIKE '%campaing_anterior\":\"{$padre_id_solo}%' AND id != {$campaing->id} ");
+				$enviados_otro = ( $otro_flujo->enviados != '' ) ? (array) json_decode($otro_flujo->enviados) : [];
+				foreach ($enviados[$padre_id] as $key => $email) {
+					if( !array_key_exists($email, $enviados_otro[$padre_id]) ){ 
+						$enviados_otro[$padre_id][ $email ] = time();
+					}
+				}
+				$data_otros = json_encode($enviados_otro, JSON_UNESCAPED_UNICODE);
+				echo $sql = "UPDATE vlz_campaing SET enviados = '{$data_otros}' WHERE id = ".$otro_flujo->id;
+
 			break;
 		}
 	}
@@ -121,9 +133,11 @@
 				$padre_id = "padre_".$data->campaing_anterior;
 				$enviados = ( $campaing->enviados != '' ) ? (array) json_decode($campaing->enviados) : [];
 
+				/*
 				echo "<pre>";
 					print_r($enviados);
 				echo "</pre><br><br>";
+				*/
 
 				switch ( $data->campaing_despues_no_abre ) {
 					case 'si':
