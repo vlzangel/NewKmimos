39d2cc06ae8b4848e414dd84fcf1352e9b4e6f71
Angel Veloz <vlzangel91@gmail.com>
2019-10-09 13:37:30 -0400

    Update cron_confirmaciones.php

--- cron/cron_confirmaciones.php
+++ cron/cron_confirmaciones.php
@@ -33,25 +33,17 @@
 
                 $reserva_id = $wpdb->get_var("SELECT ID FROM wp_posts WHERE post_parent = ".$request->ID);
                 $pre_change_status = get_post_meta($reserva_id, 'pre_change_status', true);
-                $pre_change_status = json_decode( $pre_change_status );
 
-                
+                if( !empty($pre_change_status) ){
+                    $pre_change_status = json_decode( $pre_change_status );
 
-                if( ( time() + 60 ) > $pre_change_status->hora ){
-
-                    // print_r( $pre_change_status );
-
-                    // echo date("d/m/Y H:i:s", ( time() + 60))." > ".date("d/m/Y H:i:s", $pre_change_status->hora)." <br>";
-
-                    $acc = $pre_change_status->acc; 
-                    $usu = $pre_change_status->usu;
-
-                    $_GET["CONFIRMACION"] = "YES";
-
-                    $id_orden = $request->ID;
-
-                    include( dirname(__DIR__)."/wp-content/themes/kmimos/procesos/reservar/emails/index.php" );
-                    
+                    if( ( time() + 60 ) > $pre_change_status->hora ){
+                        $acc = $pre_change_status->acc; 
+                        $usu = $pre_change_status->usu;
+                        $_GET["CONFIRMACION"] = "YES";
+                        $id_orden = $request->ID;
+                        include( dirname(__DIR__)."/wp-content/themes/kmimos/procesos/reservar/emails/index.php" );
+                    }
                 }
 
             }
