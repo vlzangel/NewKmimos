cb0600177e9864e9e7356b2902c8a8548f20b271
Angel Veloz <vlzangel91@gmail.com>
2019-10-10 12:35:58 -0400

    Act

--- wp-content/themes/kmimos/template/mail/reservar/partes/cuidadores.php
+++ wp-content/themes/kmimos/template/mail/reservar/partes/cuidadores.php
@@ -1,3 +1,43 @@
+<div style='display: inline-block;background-color: #ffffff;margin: 0px 5px 20px;width: 100%;max-width: 280px;box-sizing: border-box;padding: 10px;'>
+	<table width='100%'style='padding: 0px;'>
+		<tr>
+			<td align='center' height='160' width='100%' style='background-repeat: no-repeat; background-position: center; background-size: cover; background-color: #e8e8e8;display: flex;justify-content: center;align-items: center;'>
+				<img src="[AVATAR]" style='max-width:100%;height:160px;'>
+			</td>
+
+		</tr>
+	</table>
+	<div style='padding: 10px;'>
+		<div style=' font-family: Arial; font-size: 20px; font-weight: bold; letter-spacing: 0.4px; text-align: left; color: #000000; margin: 5px 0px 3px; text-transform: uppercase;'>
+			[NAME_CUIDADOR] 
+		</div> 
+		<div style='font-family: Verdana;font-size: 16px;letter-spacing: -0.3px;color: #000000;text-align: left;'>
+			[EXPERIENCIA]
+		</div>
+		<div style='margin: 10px 0px; text-align: left;'>
+			[HUESOS]
+		</div>
+		<div style='font-family: Arial; font-size: 13px; font-weight: bold; letter-spacing: -0.1px; text-align: left; color: #000; height: 28px; vertical-align: middle;'>
+			<span id='precio' style='font-family: Arial; font-size: 13px; font-weight: 600; letter-spacing: -0.1px; text-align: left; vertical-align: middle; display: inline-block;'>
+				DESDE MXN$ 
+				<span style='font-family: Arial; font-size: 18px;'>
+					[MONTO]
+				</span>, 
+				[MONTO_DECIMALES]
+			</span>
+		</div>
+	</div>
+	<div style='text-align: center; margin-top: 10px;'>
+		<a href='[LIKS]' target='_blank' style='text-decoration: none;display: inline-block;width: 115px;border-radius: 3px;border: solid 1px #000;padding: 12px 0px;text-align: center;margin: 0px 3px 0px 5px;color: #000;font-size: 12px;font-weight: 600;'>
+			CONOCE M&Aacute;S
+		</a>
+		<a href='[LIKS]' target='_blank' style='text-decoration: none;display: inline-block;width: 115px;border-radius: 3px;border: solid 1px #920e9d;padding: 12px 0px;text-align: center;margin: 0px 3px 0px 5px;color: #FFF;font-size: 12px;font-weight: 600;background-color: #920e9d;'>
+			RESERVA AQU&Iacute;
+		</a>
+	</div>
+</div>
+
+<?php /*
 <div style='
     display: inline-block;
     background-color: #ffffff;
@@ -99,3 +139,4 @@
 	</div>
 
 </div>
+*/ ?>
