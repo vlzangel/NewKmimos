f3d13ccf6e4dbf48cae189307ecec8fd983dd2b3
Angel Veloz <vlzangel91@gmail.com>
2020-01-17 15:12:48 -0500

    Update nuevo.php

--- kmivet/wp-content/themes/kmimos/template/mail/KMIVET/veterinario/nuevo.php
+++ kmivet/wp-content/themes/kmimos/template/mail/KMIVET/veterinario/nuevo.php
@@ -1,7 +1,15 @@
 <div style='text-align:center; margin-bottom: 0px;'>
     <img src='[KV_URL_IMGS]/VETERINARIO/NUEVO/HEADER.png' style='width: 100%;' >
 </div>
-<div style='margin-bottom: 0px; font-size: 14px; line-height: 1.07; letter-spacing: 0.3px; color: #000000; position: relative;'>
+<div style='
+    
+    font-size: 14px;
+    line-height: 1.07;
+    letter-spacing: 0.3px;
+    color: #000000;
+    position: relative;
+    margin-bottom: 20px;
+'>
     <div style='font-family: Arial; font-size: 30px; font-weight: bold; letter-spacing: 0.4px; color: #000; padding-bottom: 15px;'>
         ¡Hola!
     </div>    
@@ -32,38 +40,6 @@
         Estimado Veterinario candidato a formar parte de nuestra red, nos complace notificarle que ya estamos listos para continuar con su proceso de selección.
     </div>
 
-    <ol style='
-        width: 100%;
-        font-size: 16px;
-        line-height: 1.5;
-        margin-bottom: 15px;
-        padding-bottom: 15px;
-        text-align: justify;
-        border-bottom: solid 1px #CCC;
-    '>
-        <li>Las próximas 24 hrs nosotros te contactaremos.</li>
-
-        <li> Consulta nuestro video de capacitación sobre la aplicación en el siguiente botón. </li>
-         
-        <li> 
-            <strong>Dar click en evaluación</strong>
-            La cual consta de breves preguntas y unos casos clínicos muy sencillos.
-            <a hre="#">
-                Ir a Evaluacion
-            </a>
-        </li>
-
-        <li>Una vez aprobada la evaluación, usted recibirá una confirmación vía correo electrónico con los requerimientos para su fotografía de identidad y un código promocional para compartir con tus colegas y recibir un beneficio.</li>
-         
-        <li>
-            Recaba toda tu documentación de los documentos que estamos solicitando.
-            <a hre="#">
-                Ver lista de documentos
-            </a>
-        </li>
-        <li>Tendrás una cita con nosotros para unirte a nuestra red.</li>
-    </ol>
-
     <div style='
         width: 100%;
         font-size: 20px;
@@ -76,38 +52,79 @@
         Comenzar es muy fácil
     </div>  
 
+    <ol style='
+        font-size: 16px;
+        line-height: 1.5;
+        margin-bottom: 15px;
+        padding-bottom: 15px;
+        text-align: justify;
+        border-bottom: solid 1px #CCC;
+    '>
+        <li style="margin-bottom: 8px;">
+            En las próximas 24 hrs nosotros te contactaremos.
+        </li>
+         
+        <li style="margin-bottom: 8px;">
 
+            <strong>Dar click en evaluación</strong>
+            la cual consta de breves preguntas y unos casos clínicos muy sencillos.
+            <a hre="#" style="
+                display: block;
+                width: 200px;
+                margin: 15px auto 20px;
+                text-align: center;
+                padding: 8px 0px;
+                background-color: #fe196b;
+                color: #FFF;
+                border-radius: 5px;
+                font-weight: 600;
+                cursor: pointer;
+                text-decoration: none;
+            ">
+                Ir a Evaluacion
+            </a>
+        </li>
 
+        <li style="margin-bottom: 8px;">
+            Una vez aprobada la evaluación, usted recibirá una confirmación vía correo electrónico con los requerimientos para su fotografía de identidad y un código promocional para compartir con tus colegas y recibir un beneficio.
+        </li>
+         
+        <li style="margin-bottom: 8px;">
+            Recaba toda tu documentación de los documentos que estamos solicitando.
+            <a hre="#" style="
+                display: block;
+                width: 200px;
+                margin: 15px auto 20px;
+                text-align: center;
+                padding: 8px 20px;
+                background-color: #fe196b;
+                color: #FFF;
+                border-radius: 5px;
+                font-weight: 600;
+                cursor: pointer;
+                text-decoration: none;
+            ">
+                Ver lista de documentos
+            </a>
+        </li>
 
+        <li style="margin-bottom: 8px;">
+            Tendrás una cita con nosotros para unirte a nuestra red.
+        </li>
+    </ol>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <div style='width: 100%; font-size: 16px; line-height: 1.5;'>Te entregamos tus credenciales para que tengas acceso a Kmivet: </div>
-</div>
-
-<div style='text-align: center;'>
-    <img style="width: 85px; margin: 20px 0px;" src='[KV_URL_IMGS]/VETERINARIO/NUEVO/img_1.png' />
+    <div style='width: 100%; font-size: 16px; line-height: 1.5; text-align: justify;'>
+        A continuación te compartimos tus <strong>credenciales</strong> para que tengas acceso a <strong style="color: #fe196b;">Kmivet</strong> y puedas <strong>editar</strong> la información suministrada en caso de necesitarlo:
+    </div>
 </div>
 
 <div style='display: block; max-width: 400px; width: 100%; margin: 0px auto; margin-bottom: 10px;'>
-    <div style='display: table; margin-bottom: 31px;'>
-        <div style='display: table-cell; vertical-align: middle; padding-left: 16px;'>
-            <div style='font-size: 13px; font-weight: bold; letter-spacing: -0.1px; color: #0d7ad9;'>E-mail</div>                   
-            <div style='font-family: Arial; font-size: 13px; font-weight: bold; letter-spacing: -0.1px; color: #000000; margin-bottom: 12px;'><a href="mailto:[email]" style="color: #000; text-decoration: none;">[EMAIL]</a></div>
-            <div style='font-family: Arial; font-size: 13px; font-weight: bold; letter-spacing: -0.1px; color: #0d7ad9;'>Contraseña</div>
-            <div style='font-family: Arial; font-size: 13px; font-weight: bold; letter-spacing: -0.1px; color: #000000;'>[PASS]</div>    
+    <div style='display: block; text-align: center; margin-bottom: 31px;'>
+        <div style='display: block;'>
+            <div style='font-size: 18px; font-weight: bold; letter-spacing: -0.1px; color: #0d7ad9; margin-bottom: 2px;'>E-mail</div>                   
+            <div style='font-family: Arial; font-size: 18px; font-weight: normal; letter-spacing: -0.1px; color: #000000; margin-bottom: 12px;'><a href="mailto:[email]" style="color: #000; text-decoration: none;">[EMAIL]</a></div>
+            <div style='font-family: Arial; font-size: 18px; font-weight: bold; letter-spacing: -0.1px; color: #0d7ad9; margin-bottom: 2px;'>Contraseña</div>
+            <div style='font-family: Arial; font-size: 18px; font-weight: normal; letter-spacing: -0.1px; color: #000000;'>[PASS]</div>    
         </div>
     </div>
     <div style='text-align: center;'>
