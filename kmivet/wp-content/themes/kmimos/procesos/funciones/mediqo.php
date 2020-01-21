3e07638d1bdb223c5cbcb462b95fd97b2d4c7d8d
Angel Veloz <vlzangel91@gmail.com>
2020-01-21 17:34:53 -0500

    Update mediqo.php

--- kmivet/wp-content/themes/kmimos/procesos/funciones/mediqo.php
+++ kmivet/wp-content/themes/kmimos/procesos/funciones/mediqo.php
@@ -2,7 +2,7 @@
 	function mediqo_request($url, $params, $type = 'POST'){
 		// $url = 'https://api.mediqo.mx/'.$url;
 		// $url = '13.59.244.182/'.$url;
-		$url = '3.86.249.47/'.$url;
+		$url = 'http://3.86.249.47/'.$url;
 		
 		if( $type == 'POST' ){
 			$ch = curl_init($url);
