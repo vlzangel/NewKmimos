72542562d9a9bd0b68f4fe6b1d77a6ecfe80ce3c
Angel Veloz <vlzangel91@gmail.com>
2020-01-30 12:37:08 -0500

    Update login.php

--- kmivet/wp-content/themes/kmimos/procesos/login/login.php
+++ kmivet/wp-content/themes/kmimos/procesos/login/login.php
@@ -116,7 +116,7 @@
 						    update_user_meta($_USER_ID, '_mediqo_medic_id', $res['id']);
 							update_user_meta($_USER_ID, '_mediqo_active', time() );
 
-							$wpdb->quer("UPDATE {$wpdb->prefix}kmivet_veterinarios SET veterinario_id = '{$res['id']}', status = 1 WHERE user_id = '{$user->ID}'");
+							$wpdb->query("UPDATE {$wpdb->prefix}kmivet_veterinarios SET veterinario_id = '{$res['id']}', status = 1 WHERE user_id = '{$user->ID}'");
 
 							$mensaje = kv_get_email_html(
 					            'KMIVET/veterinario/activado', 
