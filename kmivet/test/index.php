6a9ea9cb5edad4d74f8ace4c1edab4364a6b8b03
Ángel Veloz <vlzangel91@gmail.com>
2020-02-05 10:22:57 -0500

    Update index.php

--- kmivet/test/index.php
+++ kmivet/test/index.php
@@ -1,23 +1,29 @@
-<!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="https://www.gstatic.com/firebasejs/7.8.0/firebase-app.js"></script>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
 
-<!-- TODO: Add SDKs for Firebase products that you want to use
-     https://firebase.google.com/docs/web/setup#available-libraries -->
-<script src="https://www.gstatic.com/firebasejs/7.8.0/firebase-analytics.js"></script>
+        <script src="https://www.gstatic.com/firebasejs/7.8.0/firebase-app.js"></script>
+        <script src="https://www.gstatic.com/firebasejs/7.8.0/firebase-analytics.js"></script>
+        <script>
+            var firebaseConfig = {
+                apiKey: "AIzaSyCJ3U368AbKjfn7qRZxBt5b9tWO_0yL-_M",
+                authDomain: "kmivet.firebaseapp.com",
+                databaseURL: "https://kmivet.firebaseio.com",
+                projectId: "kmivet",
+                storageBucket: "kmivet.appspot.com",
+                messagingSenderId: "816587933523",
+                appId: "1:816587933523:web:b6b5ef64997209f2f443a6",
+                measurementId: "G-WGCS06RXVE"
+            };
+            firebase.initializeApp(firebaseConfig);
+            firebase.analytics();
+        </script>
 
-<script>
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyCJ3U368AbKjfn7qRZxBt5b9tWO_0yL-_M",
-    authDomain: "kmivet.firebaseapp.com",
-    databaseURL: "https://kmivet.firebaseio.com",
-    projectId: "kmivet",
-    storageBucket: "kmivet.appspot.com",
-    messagingSenderId: "816587933523",
-    appId: "1:816587933523:web:b6b5ef64997209f2f443a6",
-    measurementId: "G-WGCS06RXVE"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-</script>
+    </head>
+    <body>
+
+    </body>
+</html>
+
+        
