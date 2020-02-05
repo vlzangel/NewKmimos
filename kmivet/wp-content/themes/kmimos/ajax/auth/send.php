79887ff59a7af6bd2b835f2a0f026a746996665d
Ángel Veloz <vlzangel91@gmail.com>
2020-02-05 14:25:27 -0500

    Update send.php

--- kmivet/wp-content/themes/kmimos/ajax/auth/send.php
+++ kmivet/wp-content/themes/kmimos/ajax/auth/send.php
@@ -1,6 +1,7 @@
 <?php
 	$user_id = $wpdb->get_var("SELECT ID FROM wp_users WHERE user_email = '{$email}' ");
 	$tipo = get_user_meta($user_id, 'tipo_usuario', true);
+	update_user_meta($user_id, '_token_notification', $uid);
 	if( $tipo == 'veterinario' ){
 		$veterinario_id = $wpdb->get_var("SELECT veterinario_id FROM wp_kmivet_veterinarios WHERE user_id = '{$user_id}' ");
 		$r = send_token_veterinario($veterinario_id, $uid);
