7d61f9fef20638b5a1049cc15d33156bd5720628
Ángel Veloz <vlzangel91@gmail.com>
2020-02-05 15:48:26 -0500

    Update firebase-init.js

--- kmivet/wp-content/themes/kmimos/js/firebase-init.js
+++ kmivet/wp-content/themes/kmimos/js/firebase-init.js
@@ -41,49 +41,50 @@ function send_uid(email, uid){
 firebase.auth().signInWithEmailAndPassword("kmivettres@mail.com", '123456').then(function(user){
 
     Notification.requestPermission().then((permission) => {
-  if (permission === 'granted') {
-    console.log('Notification permission granted.');
-    // TODO(developer): Retrieve an Instance ID token for use with FCM.
-    // ...
-  } else {
-    console.log('Unable to get permission to notify.');
-  }
-});
-    
-    const messaging = firebase.messaging();
+        if (permission === 'granted') {
+            console.log('Notification permission granted.');
 
-    messaging.usePublicVapidKey('BOKyoDDTOLAKPbgvzdg0k55N2X3866lwyRYhpPgjGmTjo561hp-fpdwp-WU1BN4FvaMDB6IsYiiJJLYRz2IQte0');
-    messaging.getToken().then((currentToken) => {
-        if (currentToken) {
-            // sendTokenToServer(currentToken);
-            // updateUIForPushEnabled(currentToken);
+            const messaging = firebase.messaging();
+
+            messaging.usePublicVapidKey('BOKyoDDTOLAKPbgvzdg0k55N2X3866lwyRYhpPgjGmTjo561hp-fpdwp-WU1BN4FvaMDB6IsYiiJJLYRz2IQte0');
+            messaging.getToken().then((currentToken) => {
+                if (currentToken) {
+                    // sendTokenToServer(currentToken);
+                    // updateUIForPushEnabled(currentToken);
+                } else {
+                    // Show permission request.
+                    console.log('No Instance ID token available. Request permission to generate one.');
+                    // Show permission UI.
+                    // updateUIForPushPermissionRequired();
+                    // setTokenSentToServer(false);
+                }
+            }).catch((err) => {
+                console.log('An error occurred while retrieving token. ', err);
+                // showToken('Error retrieving Instance ID token. ', err);
+                // setTokenSentToServer(false);
+            });
+
+            messaging.onTokenRefresh(() => {
+                messaging.getToken().then((refreshedToken) => {
+                    console.log('Token refreshed.');
+                    // setTokenSentToServer(false);
+                    // sendTokenToServer(refreshedToken);
+                }).catch((err) => {
+                    console.log('Unable to retrieve refreshed token ', err);
+                    // showToken('Unable to retrieve refreshed token ', err);
+                });
+            });
+
+            messaging.onMessage((payload) => {
+              console.log('Message received. ', payload);
+            });
+            
         } else {
-            // Show permission request.
-            console.log('No Instance ID token available. Request permission to generate one.');
-            // Show permission UI.
-            // updateUIForPushPermissionRequired();
-            // setTokenSentToServer(false);
+            console.log('Unable to get permission to notify.');
         }
-    }).catch((err) => {
-        console.log('An error occurred while retrieving token. ', err);
-        // showToken('Error retrieving Instance ID token. ', err);
-        // setTokenSentToServer(false);
     });
 
-    messaging.onTokenRefresh(() => {
-        messaging.getToken().then((refreshedToken) => {
-            console.log('Token refreshed.');
-            // setTokenSentToServer(false);
-            // sendTokenToServer(refreshedToken);
-        }).catch((err) => {
-            console.log('Unable to retrieve refreshed token ', err);
-            // showToken('Unable to retrieve refreshed token ', err);
-        });
-    });
-
-    messaging.onMessage((payload) => {
-      console.log('Message received. ', payload);
-    });
+            
 
 }).catch(function(error) {
     var errorCode = error.code;
