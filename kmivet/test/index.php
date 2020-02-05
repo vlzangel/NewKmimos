8bbdf3d98e221337b5912f9f5cb6ea9359fb1cdd
Ángel Veloz <vlzangel91@gmail.com>
2020-02-05 10:13:54 -0500

    Create index.php

---
+++ kmivet/test/index.php
@@ -0,0 +1,23 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.8.0/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/7.8.0/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCJ3U368AbKjfn7qRZxBt5b9tWO_0yL-_M",
+    authDomain: "kmivet.firebaseapp.com",
+    databaseURL: "https://kmivet.firebaseio.com",
+    projectId: "kmivet",
+    storageBucket: "kmivet.appspot.com",
+    messagingSenderId: "816587933523",
+    appId: "1:816587933523:web:b6b5ef64997209f2f443a6",
+    measurementId: "G-WGCS06RXVE"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
