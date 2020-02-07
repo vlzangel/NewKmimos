0e1067052bf5bf0f9fd06a02745352b084f3ab3a
Ángel Veloz <vlzangel91@gmail.com>
2020-02-07 15:07:34 -0500

    Update valorar.php

--- kmivet/wp-content/themes/kmimos/ajax/paciente/valorar.php
+++ kmivet/wp-content/themes/kmimos/ajax/paciente/valorar.php
@@ -1,5 +1,5 @@
 <?php
-	$rcs = change_status($cita_id, [ "status" => 5, "description" => $mensaje ]);
+	$rcs = change_status($cita_id, [ "status" => 5, "description" => $mensaje, "source" => "1" ]);
 	if( $rcs['status'] == 'ok' ){ 
 		$r = $wpdb->query("UPDATE {$pf}reservas SET status = 5, calificacion = '{$valor}', observaciones = '{$mensaje}' WHERE id = '{$id}' ");
 		if( $r ){
