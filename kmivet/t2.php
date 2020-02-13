a12fba443c938219b3e4c428734dee3ee9ac3e9b
Ángel Veloz <vlzangel91@gmail.com>
2020-02-13 11:47:33 -0500

    Update t2.php

--- kmivet/t2.php
+++ kmivet/t2.php
@@ -21,28 +21,64 @@
         "TOTAL"             => number_format(350, 2, ',', '.')
     ];
 
-    // EMAIL al CLIENTE //
+    $medicos = get_medics(
+        'edcd1f3119674477b70b4304000ffa30',
+        19.440658,
+        -99.165991
+    );
+
+    $RECOMENDACIONES = ''; $cont = 0;
+    foreach ($medicos["res"]->objects as $key => $medico) {
+        $img = ( ( $medico->profilePic ) != "" ) ? $medico->profilePic : 'http://www.psi-software.com/wp-content/uploads/2015/07/silhouette-250x250.png';
+        $RECOMENDACIONES .= buildEmailTemplate('KMIVET/partes/recomendacion', [
+            'IMG' => $img,
+            'NAME' => email_set_format_name( $medico->firstName.' '.$medico->lastName),
+            'RATE' => email_set_format_ranking( getTema().'/KMIVET/img', $medico->rating),
+            'PRECIO' => email_set_format_precio($medico->price)
+        ]);
+        $cont++;
+        if( $cont > 2 ){
+            break;
+        }
+    }
+
+    $INFORMACION['RECOMENDACIONES'] = $RECOMENDACIONES;
+
+    $mensaje = kv_get_email_html(
+        'KMIVET/reservas/'.$cancelado_por.'/cancelacion_cliente', 
+        $INFORMACION
+    );
+    wp_mail('mary.garciag@gmail.com', 'Kmivet - Consulta Cancelada', $mensaje);
+
+    $mensaje = kv_get_email_html(
+        'KMIVET/reservas/'.$cancelado_por.'/cancelacion_veterinario', 
+        $INFORMACION
+    );
+    wp_mail('mary.garciag@gmail.com', 'Kmivet - Consulta Cancelada', $mensaje);
+
+    $mensaje = kv_get_email_html(
+        'KMIVET/reservas/'.$cancelado_por.'/cancelacion_admin', 
+        $INFORMACION
+    );
+    wp_mail('mary.garciag@gmail.com', 'Kmivet - Consulta Cancelada', $mensaje);
+
+    /*
         $mensaje = kv_get_email_html(
             'KMIVET/reservas/nueva_cliente', 
             $INFORMACION
         );
         wp_mail('mary.garciag@gmail.com', 'Kmivet - Nueva Solicitud de Consulta', $mensaje);
 
-
-    // EMAIL al CUIDADOR //
         $mensaje = kv_get_email_html(
             'KMIVET/reservas/nueva_veterinario', 
             $INFORMACION
         );
         wp_mail('mary.garciag@gmail.com', 'Kmivet - Nueva Solicitud de Consulta', $mensaje);
 
-    // EMAIL al ADMINISTRADOR //
         $mensaje = kv_get_email_html(
             'KMIVET/reservas/nueva_admin', 
             $INFORMACION
         );
-        $header = kv_get_emails_admin();
-        wp_mail('mary.garciag@gmail.com', 'Kmivet - Nueva Solicitud de Consulta', $mensaje, $header);
-
-    echo $mensaje;
+        wp_mail('mary.garciag@gmail.com', 'Kmivet - Nueva Solicitud de Consulta', $mensaje);
+    */
 ?>
